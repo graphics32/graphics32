5ee7f5932e2be63a9c4398fb4499dcbacd5def62
Andre H. Beckedorf <evilJazz@users.noreply.github.com>
2005-01-12 11:38:42 +0000

    Modified to work with changes in Transformation classes.

--- GR32/Examples/Vcl/Layers/RotLayer_Ex/GR32_RotLayer.pas
+++ GR32/Examples/Vcl/Layers/RotLayer_Ex/GR32_RotLayer.pas
@@ -111,8 +111,9 @@ var
   BX, BY: Integer;
   Pt: TPoint;
 begin
-  with TATAccess(Transformation) do Transform(X, Y, BX, BY); // BX,BY - in 'FBitmap' coordinates
-  Pt := Point(BX, BY);
+  with TATAccess(Transformation) do
+    Pt := ReverseTransform(Point(X, Y)); // BX,BY - in 'FBitmap' coordinates
+  BX := Pt.X; BY := Pt.Y;
   if PtInRect(Rect(0, 0, Bitmap.Width, Bitmap.Height), Pt) then Result := True;
   if Result and AlphaHit and (Bitmap.PixelS[BX, BY] and $FF000000 = 0) then
     Result := False;
