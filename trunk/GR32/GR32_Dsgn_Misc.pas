5978b133c85cd2df75a8ca3d24b2c14a2055649e
mandersson <mandersson@dadbf757-a53b-4b3f-9c79-7c22a95d7d7a>
2005-04-05 23:26:38 +0000

    A common ancestor class for class properties has been added (TCustomClassProperty).

--- trunk/GR32/GR32_Dsgn_Misc.pas
+++ trunk/GR32/GR32_Dsgn_Misc.pas
@@ -6,61 +6,60 @@ uses
   DesignIntf, DesignEditors, Classes, TypInfo;
 
 type
-	TKernelClassProperty = class(TClassProperty)
-  private
+  TCustomClassProperty = class(TClassProperty)
+  protected
+    class function GetClassList: TList; virtual; abstract;
     function HasSubProperties: Boolean;
+    procedure SetClassName(const CustomClass: string); virtual; abstract;
+    function GetObject: TObject; virtual; abstract;
   public
 		function GetAttributes: TPropertyAttributes; override;
 		procedure GetValues(Proc: TGetStrProc); override;
-		function GetValue: string; override;
 		procedure SetValue(const Value: string); override;
+		function GetValue: string; override;
+  end;
+
+	TKernelClassProperty = class(TCustomClassProperty)
+  protected
+    class function GetClassList: TList; override;
+    procedure SetClassName(const CustomClass: string); override;
+    function GetObject: TObject; override;
 	end;
 
-	TResamplerClassProperty = class(TClassProperty)
-  private
-    function HasSubProperties: Boolean;
-  public
-		function GetAttributes: TPropertyAttributes; override;
-		procedure GetValues(Proc: TGetStrProc); override;
-		function GetValue: string; override;
-		procedure SetValue(const Value: string); override;
+	TResamplerClassProperty = class(TCustomClassProperty)
+  protected
+    class function GetClassList: TList; override;
+    procedure SetClassName(const CustomClass: string); override;
+    function GetObject: TObject; override;
 	end;
 
 implementation
 
 uses GR32, GR32_Resamplers;
 
-{ TKernelClassProperty }
+{ TCustomClassProperty }
 
-function TKernelClassProperty.HasSubProperties: Boolean;
-begin
-  if PropCount > 0 then
-    Result := GetTypeData(TKernelResampler(GetComponent(0)).Kernel.ClassInfo)^.PropCount > 0
-  else
-    Result := False;
-end;
-
-function TKernelClassProperty.GetAttributes: TPropertyAttributes;
+function TCustomClassProperty.GetAttributes: TPropertyAttributes;
 begin
   Result := inherited GetAttributes - [paReadOnly] +
     [paValueList, paRevertable, paVolatileSubProperties];
   if not HasSubProperties then Exclude(Result, paSubProperties);
 end;
 
-function TKernelClassProperty.GetValue: string;
+function TCustomClassProperty.GetValue: string;
 begin
   if PropCount > 0 then
-    Result := TKernelResampler(GetComponent(0)).KernelClassName
+    Result := GetObject.ClassName
   else
     Result := '';
 end;
 
-procedure TKernelClassProperty.GetValues(Proc: TGetStrProc);
+procedure TCustomClassProperty.GetValues(Proc: TGetStrProc);
 var
   I: Integer;
   S: TStrings;
 begin
-  S := GetKernelClassNames;
+  S := GetCustomClassNames(GetClassList);
   try
     for I := 0 to S.Count - 1 do
       Proc(S[I]);
@@ -69,68 +68,55 @@ begin
   end;
 end;
 
-procedure TKernelClassProperty.SetValue(const Value: string);
-var
-	RC: TCustomKernelClass;
+function TCustomClassProperty.HasSubProperties: Boolean;
 begin
-	RC := FindKernelClass(Value);
-	if Assigned(RC) then
-    TKernelResampler(GetComponent(0)).KernelClassName := Value
+  if PropCount > 0 then
+    Result := GetTypeData(GetObject.ClassInfo)^.PropCount > 0
+  else
+    Result := False;
+end;
+
+procedure TCustomClassProperty.SetValue(const Value: string);
+begin
+	if Assigned(FindCustomClass(Value, GetClassList)) then
+    SetClassName(Value)
 	else SetStrValue('');
 	Modified;
 end;
 
+{ TKernelClassProperty }
+
+class function TKernelClassProperty.GetClassList: TList;
+begin
+  Result := KernelList;
+end;
+
+function TKernelClassProperty.GetObject: TObject;
+begin
+  Result := TKernelResampler(GetComponent(0)).Kernel;
+end;
+
+procedure TKernelClassProperty.SetClassName(const CustomClass: string);
+begin
+  TKernelResampler(GetComponent(0)).KernelClassName := CustomClass;
+end;
 
 { TResamplerClassProperty }
 
-function TResamplerClassProperty.HasSubProperties: Boolean;
+class function TResamplerClassProperty.GetClassList: TList;
 begin
-  if PropCount > 0 then
-    Result := GetTypeData(TBitmap32(GetComponent(0)).Resampler.ClassInfo)^.PropCount > 0
-  else
-    Result := False;
+  Result := ResamplerList;
 end;
 
-function TResamplerClassProperty.GetAttributes: TPropertyAttributes;
+function TResamplerClassProperty.GetObject: TObject;
 begin
-  Result := inherited GetAttributes - [paReadOnly] +
-    [paValueList, paRevertable, paVolatileSubProperties];
-  if not HasSubProperties then Exclude(Result, paSubProperties);
+  Result := TBitmap32(GetComponent(0)).Resampler;
 end;
 
-function TResamplerClassProperty.GetValue: string;
+procedure TResamplerClassProperty.SetClassName(
+  const CustomClass: string);
 begin
-  if PropCount > 0 then
-    Result := TBitmap32(GetComponent(0)).ResamplerClassName
-  else
-    Result := '';
-end;
-
-procedure TResamplerClassProperty.GetValues(Proc: TGetStrProc);
-var
-  I: Integer;
-  S: TStrings;
-begin
-  S := GetResamplerClassNames;
-  try
-    for I := 0 to S.Count - 1 do
-      Proc(S[I]);
-  finally
-    S.Free;
-  end;
-end;
-
-procedure TResamplerClassProperty.SetValue(const Value: string);
-var
-	RC: TCustomResamplerClass;
-begin
-	RC := FindResamplerClass(Value);
-	if Assigned(RC) then
-    TBitmap32(GetComponent(0)).ResamplerClassName := Value
-	else
-    SetStrValue('');
-
-	Modified;
+  TBitmap32(GetComponent(0)).ResamplerClassName := CustomClass;
 end;
 
 end.
