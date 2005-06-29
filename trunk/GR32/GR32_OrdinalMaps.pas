9b6bcb585fb305c33590fe37a18c1f8fa2df2498
mandersson <mandersson@dadbf757-a53b-4b3f-9c79-7c22a95d7d7a>
2005-06-29 02:49:24 +0000

    Added TBooleanMap.

--- trunk/GR32/GR32_OrdinalMaps.pas
+++ trunk/GR32/GR32_OrdinalMaps.pas
@@ -1,13 +1,52 @@
 unit GR32_IntegerMaps;
 
+(* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Graphics32
+ *
+ * The Initial Developer of the Original Code is
+ * Mattias Andersson
+ *
+ * Portions created by the Initial Developer are Copyright (C) 2000-2004
+ * the Initial Developer. All Rights Reserved.
+ *
+ * ***** END LICENSE BLOCK ***** *)
+
 interface
 
 uses
-  GR32;
+  GR32; 
 
 type
   TArrayOfWord = array of Word;
   TArrayOfInteger = array of Integer;
+
+  TBooleanMap = class(TCustomMap)
+  private
+    FBits: TArrayOfByte;
+    function GetValue(X, Y: Integer): Boolean;
+    procedure SetValue(X, Y: Integer; const Value: Boolean);
+  protected
+    procedure ChangeSize(var Width, Height: Integer; NewWidth, NewHeight: Integer); override;
+  public
+    destructor Destroy; override;
+    function Empty: Boolean; override;
+    procedure Clear(FillValue: Byte);
+    procedure ToggleBit(X, Y: Integer);
+    property Value[X, Y: Integer]: Boolean read GetValue write SetValue; default;
+    property Bits: TArrayOfByte read FBits;
+  end;
 
   TWordMap = class(TCustomMap)
   private
@@ -18,10 +57,12 @@ type
   protected
     procedure ChangeSize(var Width, Height: Integer; NewWidth, NewHeight: Integer); override;
   public
+    destructor Destroy; override;
     function Empty: Boolean; override;
     procedure Clear(FillValue: Word);
     property ValPtr[X, Y: Integer]: PWord read GetValPtr;
     property Value[X, Y: Integer]: Word read GetValue write SetValue; default;
+    property Bits: TArrayOfWord read FBits;
   end;
 
   TIntegerMap = class(TCustomMap)
@@ -33,10 +74,12 @@ type
   protected
     procedure ChangeSize(var Width, Height: Integer; NewWidth, NewHeight: Integer); override;
   public
+    destructor Destroy; override;
     function Empty: Boolean; override;
     procedure Clear(FillValue: Integer);
     property ValPtr[X, Y: Integer]: PInteger read GetValPtr;
     property Value[X, Y: Integer]: Integer read GetValue write SetValue; default;
+    property Bits: TArrayOfInteger read FBits;
   end;
 
 implementation
@@ -44,6 +87,58 @@ implementation
 uses
   GR32_LowLevel;
 
+{ TBooleanMap }
+
+function Bytes(Bits: Integer): Integer;
+begin
+  Result := (Bits - 1) shr 3 + 1;
+end;
+
+procedure TBooleanMap.ChangeSize(var Width, Height: Integer; NewWidth,
+  NewHeight: Integer);
+begin
+  SetLength(FBits, Bytes(NewWidth * NewHeight));
+  Width := NewWidth;
+  Height := NewHeight;
+end;
+
+procedure TBooleanMap.Clear(FillValue: Byte);
+begin
+  FillChar(FBits[0], Bytes(Width * Height), FillValue);
+end;
+
+destructor TBooleanMap.Destroy;
+begin
+  FBits := nil;
+  inherited;
+end;
+
+function TBooleanMap.Empty: Boolean;
+begin
+  Result := not Assigned(FBits);
+end;
+
+function TBooleanMap.GetValue(X, Y: Integer): Boolean;
+begin
+  X := X + Y * Width;
+  Result := FBits[X shr 3] and (1 shl (X and 7)) <> 0; //Boolean(FBits[X shr 3] and (1 shl (X and 7)));
+end;
+
+procedure TBooleanMap.SetValue(X, Y: Integer; const Value: Boolean);
+begin
+  X := Y * Width + X;
+  if Value then
+    FBits[X shr 3] := FBits[X shr 3] or (1 shl (X and 7))
+  else
+    FBits[X shr 3] := FBits[X shr 3] and ((1 shl (X and 7)) xor $FF);
+end;
+
+procedure TBooleanMap.ToggleBit(X, Y: Integer);
+begin
+  X := Y * Width + X;
+  FBits[X shr 3] := FBits[X shr 3] xor (1 shl (X and 7));
+end;
+  
 { TWordMap }
 
 procedure TWordMap.ChangeSize(var Width, Height: Integer; NewWidth,
@@ -58,6 +153,12 @@ procedure TWordMap.Clear(FillValue: Word);
 begin
   FillWord(FBits[0], Width * Height, FillValue);
   Changed;
+end;
+
+destructor TWordMap.Destroy;
+begin
+  FBits := nil;
+  inherited;
 end;
 
 function TWordMap.Empty: Boolean;
@@ -94,6 +195,12 @@ procedure TIntegerMap.Clear(FillValue: Integer);
 begin
   FillLongword(FBits[0], Width * Height, FillValue);
   Changed;
+end;
+
+destructor TIntegerMap.Destroy;
+begin
+  FBits := nil;
+  inherited;
 end;
 
 function TIntegerMap.Empty: Boolean;
