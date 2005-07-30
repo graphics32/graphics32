192b0458d326f9546831ee728a0b5ee80f804171
pixedcom <pixedcom@dadbf757-a53b-4b3f-9c79-7c22a95d7d7a>
2005-07-30 21:34:46 +0000

    Merged GR32_ByteMaps. Updated Bits properties.

--- trunk/GR32/GR32_OrdinalMaps.pas
+++ trunk/GR32/GR32_OrdinalMaps.pas
@@ -17,24 +17,38 @@ unit GR32_OrdinalMaps;
  *
  * The Initial Developer of the Original Code is
  * Mattias Andersson
+ * (parts of this unit were merged from GR32_ByteMaps.pas by Alex A. Denisov)
  *
- * Portions created by the Initial Developer are Copyright (C) 2000-2004
+ * Portions created by the Initial Developer are Copyright (C) 2000-2005
  * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ * Michael Hansen
  *
  * ***** END LICENSE BLOCK ***** *)
 
 interface
 
+{$I GR32.inc}
+
 uses
-  GR32; 
+  {$IFDEF CLX}
+  Qt, Types, {$IFDEF LINUX}Libc, {$ENDIF}
+  QControls, QGraphics,
+  {$ELSE}
+  Windows, Controls, Graphics,
+  {$ENDIF}
+  Classes, SysUtils, GR32;
 
 type
+  TConversionType = (ctRed, ctGreen, ctBlue, ctAlpha, ctUniformRGB, ctWeightedRGB);
 
   TBooleanMap = class(TCustomMap)
   private
     FBits: TArrayOfByte;
     function GetValue(X, Y: Integer): Boolean;
     procedure SetValue(X, Y: Integer; const Value: Boolean);
+    function GetBits: PByteArray;
   protected
     procedure ChangeSize(var Width, Height: Integer; NewWidth, NewHeight: Integer); override;
   public
@@ -43,7 +57,30 @@ type
     procedure Clear(FillValue: Byte);
     procedure ToggleBit(X, Y: Integer);
     property Value[X, Y: Integer]: Boolean read GetValue write SetValue; default;
-    property Bits: TArrayOfByte read FBits;
+    property Bits: PByteArray read GetBits;
+  end;
+
+  TByteMap = class(TCustomMap)
+  private
+    FBits: TArrayOfByte;
+    function GetValue(X, Y: Integer): Byte;
+    function GetValPtr(X, Y: Integer): PByte;
+    procedure SetValue(X, Y: Integer; Value: Byte);
+    function GetBits: PByteArray;
+  protected
+    procedure AssignTo(Dst: TPersistent); override;
+    procedure ChangeSize(var Width, Height: Integer; NewWidth, NewHeight: Integer); override;
+  public
+    destructor Destroy; override;
+    procedure Assign(Source: TPersistent); override;
+    function  Empty: Boolean; override;
+    procedure Clear(FillValue: Byte);
+    procedure ReadFrom(Source: TBitmap32; Conversion: TConversionType);
+    procedure WriteTo(Dest: TBitmap32; Conversion: TConversionType); overload;
+    procedure WriteTo(Dest: TBitmap32; const Palette: TPalette32); overload;
+    property Bits: PByteArray read GetBits;
+    property ValPtr[X, Y: Integer]: PByte read GetValPtr;
+    property Value[X, Y: Integer]: Byte read GetValue write SetValue; default;
   end;
 
   TWordMap = class(TCustomMap)
@@ -52,6 +89,7 @@ type
     function GetValPtr(X, Y: Integer): PWord;
     function GetValue(X, Y: Integer): Word;
     procedure SetValue(X, Y: Integer; const Value: Word);
+    function GetBits: PWordArray;
   protected
     procedure ChangeSize(var Width, Height: Integer; NewWidth, NewHeight: Integer); override;
   public
@@ -60,7 +98,7 @@ type
     procedure Clear(FillValue: Word);
     property ValPtr[X, Y: Integer]: PWord read GetValPtr;
     property Value[X, Y: Integer]: Word read GetValue write SetValue; default;
-    property Bits: TArrayOfWord read FBits;
+    property Bits: PWordArray read GetBits;
   end;
 
   TIntegerMap = class(TCustomMap)
@@ -69,6 +107,7 @@ type
     function GetValPtr(X, Y: Integer): PInteger;
     function GetValue(X, Y: Integer): Integer;
     procedure SetValue(X, Y: Integer; const Value: Integer);
+    function GetBits: PIntegerArray;
   protected
     procedure ChangeSize(var Width, Height: Integer; NewWidth, NewHeight: Integer); override;
   public
@@ -77,7 +116,7 @@ type
     procedure Clear(FillValue: Integer);
     property ValPtr[X, Y: Integer]: PInteger read GetValPtr;
     property Value[X, Y: Integer]: Integer read GetValue write SetValue; default;
-    property Bits: TArrayOfInteger read FBits;
+    property Bits: PIntegerArray read GetBits;
   end;
 
 implementation
@@ -116,6 +155,11 @@ begin
   Result := not Assigned(FBits);
 end;
 
+function TBooleanMap.GetBits: PByteArray;
+begin
+  Result := @FBits[0];
+end;
+
 function TBooleanMap.GetValue(X, Y: Integer): Boolean;
 begin
   X := X + Y * Width;
@@ -135,6 +179,281 @@ procedure TBooleanMap.ToggleBit(X, Y: Integer);
 begin
   X := Y * Width + X;
   FBits[X shr 3] := FBits[X shr 3] xor (1 shl (X and 7));
+end;
+
+{ TByteMap }
+
+procedure TByteMap.Assign(Source: TPersistent);
+begin
+  BeginUpdate;
+  try
+    if Source is TByteMap then
+    begin
+      inherited SetSize(TByteMap(Source).Width, TByteMap(Source).Height);
+      Move(TByteMap(Source).Bits[0], Bits[0], Width * Height);
+    end
+    else if Source is TBitmap32 then
+      ReadFrom(TBitmap32(Source), ctWeightedRGB)
+    else
+      inherited;
+  finally
+    EndUpdate;
+    Changed;
+  end;
+end;
+
+procedure TByteMap.AssignTo(Dst: TPersistent);
+begin
+  if Dst is TBitmap32 then WriteTo(TBitmap32(Dst), ctUniformRGB)
+  else inherited;
+end;
+
+procedure TByteMap.ChangeSize(var Width, Height: Integer; NewWidth, NewHeight: Integer);
+begin
+  SetLength(FBits, NewWidth * NewHeight);
+  Width := NewWidth;
+  Height := NewHeight;
+end;
+
+procedure TByteMap.Clear(FillValue: Byte);
+begin
+  FillChar(Bits[0], Width * Height, FillValue);
+  Changed;
+end;
+
+destructor TByteMap.Destroy;
+begin
+  FBits := nil;
+  inherited;
+end;
+
+function TByteMap.Empty: Boolean;
+begin
+  Result := false;
+  if (Width = 0) or (Height = 0) or (FBits = nil) then Result := True;
+end;
+
+function TByteMap.GetBits: PByteArray;
+begin
+  Result := @FBits[0];
+end;
+
+function TByteMap.GetValPtr(X, Y: Integer): PByte;
+begin
+  Result := @FBits[X + Y * Width];
+end;
+
+function TByteMap.GetValue(X, Y: Integer): Byte;
+begin
+  Result := FBits[X + Y * Width];
+end;
+
+procedure TByteMap.ReadFrom(Source: TBitmap32; Conversion: TConversionType);
+var
+  W, H, I, N: Integer;
+  SrcC: PColor32;
+  SrcB, DstB: PByte;
+  Value: TColor32;
+begin
+  BeginUpdate;
+  try
+    SetSize(Source.Width, Source.Height);
+    if Empty then Exit;
+
+    W := Source.Width;
+    H := Source.Height;
+    N := W * H - 1;
+    SrcC := Source.PixelPtr[0, 0];
+    SrcB := Pointer(SrcC);
+    DstB := @FBits[0];
+    case Conversion of
+
+      ctRed:
+        begin
+          Inc(SrcB, 2);
+          for I := 0 to N do
+          begin
+            DstB^ := SrcB^;
+            Inc(DstB);
+            Inc(SrcB, 4);
+          end;
+        end;
+
+      ctGreen:
+        begin
+          Inc(SrcB, 1);
+          for I := 0 to N do
+          begin
+            DstB^ := SrcB^;
+            Inc(DstB);
+            Inc(SrcB, 4);
+          end;
+        end;
+
+      ctBlue:
+        begin
+          for I := 0 to N do
+          begin
+            DstB^ := SrcB^;
+            Inc(DstB);
+            Inc(SrcB, 4);
+          end;
+        end;
+
+      ctAlpha:
+        begin
+          Inc(SrcB, 3);
+          for I := 0 to N do
+          begin
+            DstB^ := SrcB^;
+            Inc(DstB);
+            Inc(SrcB, 4);
+          end;
+        end;
+
+      ctUniformRGB:
+        begin
+          for I := 0 to N do
+          begin
+            Value := SrcC^;
+            Value := (Value and $00FF0000) shr 16 + (Value and $0000FF00) shr 8 +
+              (Value and $000000FF);
+            Value := Value div 3;
+            DstB^ := Value;
+            Inc(DstB);
+            Inc(SrcC);
+          end;
+        end;
+
+      ctWeightedRGB:
+        begin
+          for I := 0 to N do
+          begin
+            DstB^ := Intensity(SrcC^);
+            Inc(DstB);
+            Inc(SrcC);
+          end;
+        end;
+    end;
+  finally
+    EndUpdate;
+    Changed;
+  end;
+end;
+
+procedure TByteMap.SetValue(X, Y: Integer; Value: Byte);
+begin
+  FBits[X + Y * Width] := Value;
+end;
+
+procedure TByteMap.WriteTo(Dest: TBitmap32; Conversion: TConversionType);
+var
+  W, H, I, N: Integer;
+  DstC: PColor32;
+  DstB, SrcB: PByte;
+  Resized: Boolean;
+begin
+  Dest.BeginUpdate;
+  Resized := False;
+  try
+    Resized := Dest.SetSize(Width, Height);
+    if Empty then Exit;
+
+    W := Width;
+    H := Height;
+    N := W * H - 1;
+    DstC := Dest.PixelPtr[0, 0];
+    DstB := Pointer(DstC);
+    SrcB := @FBits[0];
+    case Conversion of
+
+      ctRed:
+        begin
+          Inc(DstB, 2);
+          for I := 0 to N do
+          begin
+            DstB^ := SrcB^;
+            Inc(DstB, 4);
+            Inc(SrcB);
+          end;
+        end;
+
+      ctGreen:
+        begin
+          Inc(DstB, 1);
+          for I := 0 to N do
+          begin
+            DstB^ := SrcB^;
+            Inc(DstB, 4);
+            Inc(SrcB);
+          end;
+        end;
+
+      ctBlue:
+        begin
+          for I := 0 to N do
+          begin
+            DstB^ := SrcB^;
+            Inc(DstB, 4);
+            Inc(SrcB);
+          end;
+        end;
+
+      ctAlpha:
+        begin
+          Inc(DstB, 3);
+          for I := 0 to N do
+          begin
+            DstB^ := SrcB^;
+            Inc(DstB, 4);
+            Inc(SrcB);
+          end;
+        end;
+
+      ctUniformRGB, ctWeightedRGB:
+        begin
+          for I := 0 to N do
+          begin
+            DstC^ := Gray32(SrcB^);
+            Inc(DstC);
+            Inc(SrcB);
+          end;
+        end;
+    end;
+  finally
+    Dest.EndUpdate;
+    Dest.Changed;
+    if Resized then Dest.Resized;
+  end;
+end;
+
+procedure TByteMap.WriteTo(Dest: TBitmap32; const Palette: TPalette32);
+var
+  W, H, I, N: Integer;
+  DstC: PColor32;
+  SrcB: PByte;
+begin
+  Dest.BeginUpdate;
+  try
+    Dest.SetSize(Width, Height);
+    if Empty then Exit;
+
+    W := Width;
+    H := Height;
+    N := W * H - 1;
+    DstC := Dest.PixelPtr[0, 0];
+    SrcB := @FBits[0];
+
+    for I := 0 to N do
+    begin
+      DstC^ := Palette[SrcB^];
+      Inc(DstC);
+      Inc(SrcB);
+    end;
+  finally
+    Dest.EndUpdate;
+    Dest.Changed;
+  end;
 end;
   
 { TWordMap }
@@ -162,6 +481,11 @@ end;
 function TWordMap.Empty: Boolean;
 begin
   Result := not Assigned(FBits);
+end;
+
+function TWordMap.GetBits: PWordArray;
+begin
+  Result := @FBits[0];
 end;
 
 function TWordMap.GetValPtr(X, Y: Integer): PWord;
@@ -204,6 +528,11 @@ end;
 function TIntegerMap.Empty: Boolean;
 begin
   Result := not Assigned(FBits);
+end;
+
+function TIntegerMap.GetBits: PIntegerArray;
+begin
+  Result := @FBits[0];
 end;
 
 function TIntegerMap.GetValPtr(X, Y: Integer): PInteger;
