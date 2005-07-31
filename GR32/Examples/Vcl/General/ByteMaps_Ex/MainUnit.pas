cbffddab2a578969b1a7a01ab75f10a18f194cd3
pixedcom <info@pixed.com>
2005-07-31 14:56:02 +0000

    Updated to use the GR32_OrdinalMaps unit

--- GR32/Examples/Vcl/General/ByteMaps_Ex/MainUnit.pas
+++ GR32/Examples/Vcl/General/ByteMaps_Ex/MainUnit.pas
@@ -29,7 +29,7 @@ interface
 
 uses
   Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
-  ExtCtrls, StdCtrls, ComCtrls, Math, Clipbrd, ExtDlgs, GR32, GR32_ByteMaps,
+  ExtCtrls, StdCtrls, ComCtrls, Math, Clipbrd, ExtDlgs, GR32, GR32_OrdinalMaps,
   GR32_RangeBars, GR32_Image, GR32_Layers, ToolWin, ImgList, Menus, JPeg;
 
 type
