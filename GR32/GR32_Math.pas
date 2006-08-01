88c3b8c1e67d3555bd1ad7713474261e18061ab4
mandersson <meanderix@gmail.com>
2006-08-01 13:09:28 +0000

    Updated NextPowerOf2 and added PrevPowerOf2 and OneOver

--- GR32/GR32_Math.pas
+++ GR32/GR32_Math.pas
@@ -38,11 +38,13 @@ function FixedFloor(A: TFixed): Integer;
 function FixedCeil(A: TFixed): Integer;
 function FixedMul(A, B: TFixed): TFixed;
 function FixedDiv(A, B: TFixed): TFixed;
+function OneOver(Value: TFixed): TFixed;
 function FixedRound(A: TFixed): Integer;
 function FixedSqr(Value: TFixed): TFixed;
 function FixedSqrtLP(Value: TFixed): TFixed;      // 8-bit precision
 function FixedSqrtHP(Value: TFixed): TFixed;      // 16-bit precision
-function FixedCombine(W, X, Y: TFixed): TFixed;   // Fixed point interpolation
+// Fixed point interpolation
+function FixedCombine(W, X, Y: TFixed): TFixed; 
 
 { Trigonometric routines }
 procedure SinCos(const Theta: Single; var Sin, Cos: Single); overload;
@@ -54,10 +56,11 @@ procedure SinCos(const Theta, Radius: Single; var Sin, Cos: Single); overload;
 function MulDiv(Multiplicand, Multiplier, Divisor: Integer): Integer;
 
 //tells if X is a power of 2, returns true when X = 1,2,4,8,16 etc.
-function IsPowerOf2(X: Integer): Boolean;
-
-//returns X rounded up to next power of two, i.e. 5 -> 8, 7 -> 8, 15 -> 16
-function NextPowerOf2(X: Integer): Integer;
+function IsPowerOf2(Value: Integer): Boolean;
+//returns X rounded down to the nearest power of two
+function PrevPowerOf2(Value: Integer): Integer;
+//returns X rounded down to the nearest power of two, i.e. 5 -> 8, 7 -> 8, 15 -> 16
+function NextPowerOf2(Value: Integer): Integer;
 
 //fast average without overflow, useful for e.g. fixed point math
 function Average(A, B: Integer): Integer;
@@ -96,6 +99,14 @@ asm
         CDQ
         SHLD    EDX, EAX, 16
         SHL     EAX, 16
+        IDIV    ECX
+end;
+
+function OneOver(Value: TFixed): TFixed;
+asm
+        MOV     ECX,EAX
+        XOR     EAX,EAX
+        MOV     EDX,1
         IDIV    ECX
 end;
 
@@ -251,36 +262,43 @@ asm
         POP     EBX             // esi and ebx
 end;
 
-function IsPowerOf2(X: Integer): Boolean;
+function IsPowerOf2(Value: Integer): Boolean;
 //returns true when X = 1,2,4,8,16 etc.
 asm
-   LEA   EDX, [EAX -1]
-   AND   EAX, EDX
-   SETZ  AL
+        LEA     EDX,[EAX-1]
+        AND     EAX,EDX
+        SETZ    AL
+end;
+
+function PrevPowerOf2(Value: Integer): Integer;
+//returns X rounded down to the power of two
+asm
+        BSR     ECX,EAX
+        SHR     EAX,CL
+        SHL     EAX,CL
 end;
 
 function NextPowerOf2(X: Integer): Integer;
-//returns X rounded up to next power of two, i.e. 5 -> 8, 7 -> 8, 15 -> 16
+//returns X rounded up to the power of two, i.e. 5 -> 8, 7 -> 8, 15 -> 16
 asm
-    MOV   EDX, EAX
-    BSR   ECX, EDX
-    MOV   EAX, 1
-    SHL   EAX, CL
-    CMP   EAX, EDX
-    JE    @1
-    SHL   EAX, 1
-@1:
+        DEC     EAX
+        JLE     @1
+        BSR     ECX,EAX
+        MOV     EAX,2
+        SHL     EAX,CL
+        RET
+@1:     MOV     EAX,1
 end;
 
 function Average(A, B: Integer): Integer;
 //fast average without overflow, useful e.g. for fixed point math
 //(A + B)/2 = (A and B) + (A xor B)/2
 asm
-    MOV ECX, EDX
-    XOR EDX, EAX
-    SAR EDX, 1
-    AND EAX, ECX
-    ADD EAX, EDX
+        MOV     ECX,EDX
+        XOR     EDX,EAX
+        SAR     EDX,1
+        AND     EAX,ECX
+        ADD     EAX,EDX
 end;
 
 end.
