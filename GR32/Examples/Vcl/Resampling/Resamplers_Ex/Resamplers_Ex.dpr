eb8f80aa15a897ec13dcd8ddb42ed2bb652ccadc
Christian Budde <Christian@savioursofsoul.de>
2007-05-03 01:22:32 +0000

    Fixed bug in unassigned wrap mode and added a parameter gaugebar for additional parameters of TKernelResamplers

--- GR32/Examples/Vcl/Resampling/Resamplers_Ex/Resamplers_Ex.dpr
+++ GR32/Examples/Vcl/Resampling/Resamplers_Ex/Resamplers_Ex.dpr
@@ -2,12 +2,12 @@ program Resamplers_Ex;
 
 uses
   Forms,
-  MainUnit in 'MainUnit.pas' {Form1};
+  MainUnit in 'MainUnit.pas' {fmResamplersExample};
 
 {$R *.res}
 
 begin
   Application.Initialize;
-  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TfmResamplersExample, fmResamplersExample);
   Application.Run;
 end.
