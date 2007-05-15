7d616a41cffa4c2574f74019697f184bcc7313a4
pixedcom <pixedcom@dadbf757-a53b-4b3f-9c79-7c22a95d7d7a>
2007-05-15 15:22:02 +0000

    Minor fix again

--- trunk/GR32/GR32_Geometry.pas
+++ trunk/GR32/GR32_Geometry.pas
@@ -49,7 +49,7 @@ function Divide(const V: TFloatVector; Divisor: TFloat): TFloatVector; overload;
 function Divide(const V1, V2: TFloatVector): TFloatVector; overload;{$IFDEF USEINLINING} inline; {$ENDIF}
 
 function Combine(const V1, V2: TFloatVector; W: TFloat): TFloatVector; overload;{$IFDEF USEINLINING} inline; {$ENDIF}
-function AbsV(const V: TFloatVector): TFloatVector; overload{$IFDEF USEINLINING} inline; {$ENDIF}
+function AbsV(const V: TFloatVector): TFloatVector; overload;{$IFDEF USEINLINING} inline; {$ENDIF}
 function Neg(const V: TFloatVector): TFloatVector; overload;{$IFDEF USEINLINING} inline; {$ENDIF}
 function Average(const V1, V2: TFloatVector): TFloatVector; overload;{$IFDEF USEINLINING} inline; {$ENDIF}
 function Max(const V1, V2: TFloatVector): TFloatVector; overload;{$IFDEF USEINLINING} inline; {$ENDIF}
@@ -70,7 +70,7 @@ function Divide(const V: TFixedVector; Divisor: TFixed): TFixedVector; overload;
 function Divide(const V1, V2: TFixedVector): TFixedVector; overload;{$IFDEF USEINLINING} inline; {$ENDIF}
 
 function Combine(const V1, V2: TFixedVector; W: TFixed): TFixedVector; overload;{$IFDEF USEINLINING} inline; {$ENDIF}
-function AbsV(const V: TFixedVector): TFixedVector; overload{$IFDEF USEINLINING} inline; {$ENDIF}
+function AbsV(const V: TFixedVector): TFixedVector; overload;{$IFDEF USEINLINING} inline; {$ENDIF}
 function Neg(const V: TFixedVector): TFixedVector; overload;{$IFDEF USEINLINING} inline; {$ENDIF}
 function Average(const V1, V2: TFixedVector): TFixedVector; overload;{$IFDEF USEINLINING} inline; {$ENDIF}
 function Max(const V1, V2: TFixedVector): TFixedVector; overload;{$IFDEF USEINLINING} inline; {$ENDIF}
