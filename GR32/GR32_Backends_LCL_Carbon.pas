55e0ceced94b41c823723927600a2d9ea7df2b17
sekelsenmat <felipemonteiro.carvalho@gmail.com>
2007-06-08 19:40:24 +0000

    Lot's of improvements on the carbon implementation

--- GR32/GR32_Backends_LCL_Carbon.pas
+++ GR32/GR32_Backends_LCL_Carbon.pas
@@ -31,9 +31,17 @@ interface
 {$I GR32.inc}
 
 uses
-  LCLIntf, LCLType, types, Controls, SysUtils, Classes,
-  Graphics, GR32, GR32_Backends, GR32_Image,
-  FPCMacOSAll;
+  { RTL and LCL }
+  LCLIntf, LCLType, types, Controls, SysUtils, Classes, Graphics,
+  { Graphics 32 }
+  GR32, GR32_Backends, GR32_Containers, GR32_Image,
+  { Carbon bindings }
+  FPCMacOSAll,
+  { Carbon lcl interface }
+  CarbonCanvas, CarbonPrivate;
+
+const
+  STR_GenericRGBProfilePath = '/System/Library/ColorSync/Profiles/Generic RGB Profile.icc';
 
 type
 
@@ -49,9 +57,18 @@ type
     FOnCanvasChange: TNotifyEvent;
 
     { Carbon specific variables }
-    FPixbuf: PGdkPixBuf;
+    Stride: Integer;
+    FWidth, FHeight: Cardinal;
+    FProfile: CMProfileRef;
+    FColorSpace: CGColorSpaceRef;
+    FContext, OldCanvasContext: CGContextRef;
+    FCanvasHandle: TCarbonDeviceContext;
 
     procedure FontChangedHandler(Sender: TObject);
+
+    { Functions to easely generate carbon structures }
+    function GetCarbonRect(Left, Top, Width, Height: Integer): FPCMacOSAll.Rect;
+    function GetCGRect(Left, Top, Width, Height: Integer): FPCMacOSAll.CGRect;
   protected
     FontHandle: HFont;
 
@@ -120,24 +137,72 @@ var
 
 { TLCLBackend }
 
-constructor TLCLBackend.Create;
+procedure TLCLBackend.FontChangedHandler(Sender: TObject);
 begin
+  FontHandle := 0;
+end;
+
+function TLCLBackend.GetCarbonRect(Left, Top, Width, Height: Integer): FPCMacOSAll.Rect;
+begin
+  Result.Left := Left;
+  Result.Top := Top;
+  Result.Right := Left + Width;
+  Result.Bottom := Top + Height;
+end;
+
+function TLCLBackend.GetCGRect(Left, Top, Width, Height: Integer): FPCMacOSAll.CGRect;
+begin
+  Result.Origin.X := Left;
+  Result.Origin.Y := Top;
+  Result.Size.Width := Width;
+  Result.Size.Height := Height;
+end;
+
+constructor TLCLBackend.Create;
+var
+  loc: CMProfileLocation;
+  status: OSStatus;
+begin
+  {$IFDEF VerboseGR32Carbon}
+    WriteLn('[TLCLBackend.Create]',
+     ' Self: ', IntToHex(PtrUInt(Self), 8));
+  {$ENDIF}
+
   inherited;
+
+  { Creates a standard font }
 
   FFont := TFont.Create;
   FFont.OnChange := FontChangedHandler;
+  
+  { Creates a generic color profile }
+
+  loc.locType := cmPathBasedProfile;
+  loc.u.pathLoc.path := STR_GenericRGBProfilePath;
+  
+  status := CMOpenProfile(FProfile, loc);
+  
+  if status <> noErr then raise Exception.Create('Couldn''t create the generic profile');
+  
+  { Creates a generic color space }
+  
+  FColorSpace := CGColorSpaceCreateWithPlatformColorSpace(FProfile);
+  
+  if FColorSpace = nil then raise Exception.Create('Couldn''t create the generic RGB color space');
 end;
 
 destructor TLCLBackend.Destroy;
 begin
+  {$IFDEF VerboseGR32Carbon}
+    WriteLn('[TLCLBackend.Destroy]',
+    ' Self: ', IntToHex(PtrUInt(Self), 8));
+  {$ENDIF}
+
   FFont.Free;
 
+  CMCloseProfile(FProfile);
+  
   inherited;
-end;
-
-procedure TLCLBackend.FontChangedHandler(Sender: TObject);
-begin
-  FontHandle := 0;
 end;
 
 function TLCLBackend.GetBits: PColor32Array;
@@ -146,34 +211,64 @@ begin
 end;
 
 procedure TLCLBackend.InitializeSurface(NewWidth, NewHeight: Integer; ClearBuffer: Boolean);
-//var
-//  Stride: Integer;
 begin
-  { We allocate our own memory for the image, because otherwise it's
-    not guaranteed which Stride Gdk will use. }
-//  Stride := NewWidth * 4;
- // FBits := GetMem(NewHeight * Stride);
+  {$IFDEF VerboseGR32Carbon}
+    WriteLn('[TLCLBackend.InitializeSurface] BEGIN',
+     ' Self: ', IntToHex(PtrUInt(Self), 8),
+     ' NewWidth: ', NewWidth,
+     ' NewHeight: ', NewHeight
+     );
+  {$ENDIF}
 
-//  if FBits = nil then
-//    raise Exception.Create('Can''t allocate memory for the DIB');
+  { We allocate our own memory for the image }
+  Stride := NewWidth * 4;
+  FBits := System.GetMem(NewHeight * Stride);
 
-  { We didn't pass a memory freeing function, so we will have to take
-    care of that ourselves }
-//  FPixbuf := gdk_pixbuf_new_from_data(pguchar(FBits),
-//   GDK_COLORSPACE_RGB, True, 8, NewWidth, NewHeight, Stride, nil, nil);
+  if FBits = nil then
+  begin
+    WriteLn('[TLCLBackend.InitializeSurface] ERROR FBits = nil');
+    Exit;
+  end;
 
-//  if FPixbuf = nil then
-//    raise Exception.Create('Can''t allocate the Pixbuf');
+  { Creates a device context for our raw image area }
+
+  FContext := CGBitmapContextCreate(FBits,
+   NewWidth, NewHeight, 8, Stride, FColorSpace, kCGImageAlphaNoneSkipFirst or kCGBitmapByteOrder32Little);
+
+  if FContext = nil then
+  begin
+    WriteLn('[TLCLBackend.InitializeSurface] ERROR FContext = nil');
+    Exit;
+  end;
+
+  { flip and offset CTM to upper left corner }
+  CGContextTranslateCTM(FContext, 0, NewHeight);
+  CGContextScaleCTM(FContext, 1, -1);
+
+  FWidth := NewWidth;
+  FHeight := NewHeight;
 
   { clear the image }
-//  if ClearBuffer then
-//    FillLongword(FBits[0], NewWidth * NewHeight, clBlack32);
+  if ClearBuffer then
+    FillLongword(FBits[0], NewWidth * NewHeight, clBlack32);
+
+  {$IFDEF VerboseGR32Carbon}
+    WriteLn('[TLCLBackend.InitializeSurface] END');
+  {$ENDIF}
 end;
 
 procedure TLCLBackend.FinalizeSurface;
 begin
-//  if Assigned(FBits) then FreeMem(FBits);
-//  FBits := nil;
+  {$IFDEF VerboseGR32Carbon}
+    WriteLn('[TLCLBackend.FinalizeSurface]',
+     ' Self: ', IntToHex(PtrUInt(Self), 8));
+  {$ENDIF}
+
+  if Assigned(FBits) then System.FreeMem(FBits);
+  FBits := nil;
+  
+  if Assigned(FContext) then CGContextRelease(FContext);
+  FContext := nil;
 end;
 
 procedure TLCLBackend.Changed;
@@ -183,7 +278,7 @@ end;
 
 function TLCLBackend.Empty: Boolean;
 begin
-  Result := (FPixBuf = nil) or (FBits = nil);
+  Result := (FContext = nil) or (FBits = nil);
 end;
 
 { IPaintSupport }
@@ -199,9 +294,19 @@ begin
 end;
 
 procedure TLCLBackend.DoPaint(ABuffer: TBitmap32; AInvalidRects: TRectList;
-  ACanvas: TCanvas);
+  ACanvas: TCanvas; APaintBox: TCustomPaintBox32);
 begin
+  {  CGContextDrawImage is also possible, but it doesn't flip the image }
+   
+  HIViewDrawCGImage(
+    TCarbonDeviceContext(ACanvas.Handle).CGContext,
+    GetCGRect(0, 0, FWidth, FHeight),
+    CGBitmapContextCreateImage(FContext));
 
+  {$IFDEF VerboseGR32Carbon}
+    WriteLn('[TLCLBackend.DoPaint]',
+     ' Self: ', IntToHex(PtrUInt(Self), 8));
+  {$ENDIF}
 end;
 
 { ICopyFromBitmapSupport }
@@ -215,25 +320,57 @@ end;
 
 procedure TLCLBackend.Textout(X, Y: Integer; const Text: string);
 begin
+  {$IFDEF VerboseGR32Carbon}
+    WriteLn('[TLCLBackend.Textout]',
+     ' Self: ', IntToHex(PtrUInt(Self), 8));
+  {$ENDIF}
 
+  if not Assigned(FCanvas) then GetCanvas;
+
+  FCanvas.TextOut(X, Y, Text);
 end;
 
 procedure TLCLBackend.Textout(X, Y: Integer; const ClipRect: TRect; const Text: string);
 begin
+  {$IFDEF VerboseGR32Carbon}
+    WriteLn('[TLCLBackend.Textout with ClipRect]',
+     ' Self: ', IntToHex(PtrUInt(Self), 8));
+  {$ENDIF}
+  
+  if not Assigned(FCanvas) then GetCanvas;
 
+//  FCanvas.TextOut(DstRect.Left, DstRect.Top, Text);  simple implementation that ignores DstRect and Flags
+
+  LCLIntf.ExtTextOut(FCanvas.Handle, X, Y, ETO_CLIPPED, @ClipRect, PChar(Text), Length(Text), nil);
 end;
 
 procedure TLCLBackend.Textout(DstRect: TRect; const Flags: Cardinal; const Text: string);
 begin
+  {$IFDEF VerboseGR32Carbon}
+    WriteLn('[TLCLBackend.Textout with Flags]',
+     ' Self: ', IntToHex(PtrUInt(Self), 8));
+  {$ENDIF}
+  
+  if not Assigned(FCanvas) then GetCanvas;
 
+//  FCanvas.TextOut(DstRect.Left, DstRect.Top, Text);  simple implementation that ignores DstRect and Flags
+
+  LCLIntf.DrawText(FCanvas.Handle, PChar(Text), Length(Text), DstRect, Flags);
 end;
 
 function TLCLBackend.TextExtent(const Text: string): TSize;
 begin
+  {$IFDEF VerboseGR32Carbon}
+    WriteLn('[TLCLBackend.TextExtent]',
+     ' Self: ', IntToHex(PtrUInt(Self), 8));
+  {$ENDIF}
 
+  if not Assigned(FCanvas) then GetCanvas;
+
+  Result := FCanvas.TextExtent(Text);
 end;
 
-{ Gtk uses UTF-8, so all W functions are converted to UTF-8 ones }
+{ Carbon uses UTF-8, so all W functions are converted to UTF-8 ones }
 
 procedure TLCLBackend.TextoutW(X, Y: Integer; const Text: Widestring);
 begin
@@ -259,26 +396,51 @@ end;
 
 function TLCLBackend.GetOnFontChange: TNotifyEvent;
 begin
+  {$IFDEF VerboseGR32Carbon}
+    WriteLn('[TLCLBackend.GetOnFontChange]',
+     ' Self: ', IntToHex(PtrUInt(Self), 8));
+  {$ENDIF}
+
   Result := FOnFontChange;
 end;
 
 procedure TLCLBackend.SetOnFontChange(Handler: TNotifyEvent);
 begin
+  {$IFDEF VerboseGR32Carbon}
+    WriteLn('[TLCLBackend.SetOnFontChange]',
+     ' Self: ', IntToHex(PtrUInt(Self), 8));
+  {$ENDIF}
+
   FOnFontChange := Handler;
 end;
 
 function TLCLBackend.GetFont: TFont;
 begin
+  {$IFDEF VerboseGR32Carbon}
+    WriteLn('[TLCLBackend.GetFont]',
+     ' Self: ', IntToHex(PtrUInt(Self), 8));
+  {$ENDIF}
+
   Result := FFont;
 end;
 
 procedure TLCLBackend.SetFont(const Font: TFont);
 begin
+  {$IFDEF VerboseGR32Carbon}
+    WriteLn('[TLCLBackend.SetFont]',
+     ' Self: ', IntToHex(PtrUInt(Self), 8));
+  {$ENDIF}
+
   FFont.Assign(Font);
 end;
 
 procedure TLCLBackend.UpdateFont;
 begin
+  {$IFDEF VerboseGR32Carbon}
+    WriteLn('[TLCLBackend.UpdateFont]',
+     ' Self: ', IntToHex(PtrUInt(Self), 8));
+  {$ENDIF}
+
   FontHandle := Font.Handle;
 end;
 
@@ -286,27 +448,76 @@ end;
 
 function TLCLBackend.GetCanvasChange: TNotifyEvent;
 begin
-  Exit;
+  {$IFDEF VerboseGR32Carbon}
+    WriteLn('[TLCLBackend.GetCanvasChange]',
+     ' Self: ', IntToHex(PtrUInt(Self), 8));
+  {$ENDIF}
+
+  Result := FOnCanvasChange;
 end;
 
 procedure TLCLBackend.SetCanvasChange(Handler: TNotifyEvent);
 begin
-  Exit;
+  {$IFDEF VerboseGR32Carbon}
+    WriteLn('[TLCLBackend.SetCanvasChange]',
+     ' Self: ', IntToHex(PtrUInt(Self), 8));
+  {$ENDIF}
+
+  FOnCanvasChange := Handler;
 end;
 
 function TLCLBackend.GetCanvas: TCanvas;
 begin
-  Exit;
+  {$IFDEF VerboseGR32Carbon}
+    WriteLn('[TLCLBackend.GetCanvas] BEGIN',
+     ' Self: ', IntToHex(PtrUInt(Self), 8));
+  {$ENDIF}
+
+  if FCanvas = nil then
+  begin
+    FCanvas := TCanvas.Create;
+
+    FCanvasHandle := TCarbonDeviceContext.Create;
+    FCanvasHandle.CGContext := FContext;
+
+    FCanvas.Handle := HDC(FCanvasHandle);
+    FCanvas.OnChange := FOnCanvasChange;
+  end;
+  
+  Result := FCanvas;
+
+  {$IFDEF VerboseGR32Carbon}
+    WriteLn('[TLCLBackend.GetCanvas] END');
+  {$ENDIF}
 end;
 
 procedure TLCLBackend.DeleteCanvas;
 begin
-  Exit;
+  {$IFDEF VerboseGR32Carbon}
+    WriteLn('[TLCLBackend.DeleteCanvas]',
+     ' Self: ', IntToHex(PtrUInt(Self), 8),
+     ' FCanvas: ', PtrUInt(FCanvas));
+  {$ENDIF}
+
+  if Assigned(FCanvas) then
+  begin
+    FCanvas.Handle := 0;
+
+    FCanvas.Free;
+    
+    FCanvas := nil;
+  end;
 end;
 
 function TLCLBackend.CanvasAllocated: Boolean;
 begin
-  Exit;
+  Result := (FCanvas <> nil);
+
+  {$IFDEF VerboseGR32Carbon}
+    WriteLn('[TLCLBackend.CanvasAllocated]',
+     ' Self: ', IntToHex(PtrUInt(Self), 8),
+     ' FCanvas: ', PtrUInt(FCanvas));
+  {$ENDIF}
 end;
 
 initialization
