ed2c3938d1f5917df489e7928fe84e04726726ab
pixedcom <info@pixed.com>
2007-06-16 16:13:22 +0000

    Added support for enumeration types.

--- GR32/Examples/Vcl/Resampling/NestedSampling_Ex/SimplePropEdit.pas
+++ GR32/Examples/Vcl/Resampling/NestedSampling_Ex/SimplePropEdit.pas
@@ -63,6 +63,7 @@ type
     function GetControlClass(Kind: TTypeKind): TPersistentClass;
     procedure ButtonHandler(Sender: TObject);
     procedure TrackBarHandler(Sender: TObject);
+    procedure ComboBoxHandler(Sender: TObject);
     procedure StringGridEditHandler(Sender: TObject; ACol, ARow: Longint; const Value: string);
     procedure WMSelectObject(var Msg: TWMSelectObject); message WM_SELECTOBJECT;
   protected
@@ -97,6 +98,20 @@ begin
   PropInfo := FProps^[TComponent(Sender).Tag];
   Obj := TPersistent(GetObjectProp(FSelectedObject, PropInfo.Name));
   PostMessage(Handle, WM_SELECTOBJECT, 0, Integer(Obj));
+end;
+
+procedure TSimplePropertyEditor.ComboBoxHandler(Sender: TObject);
+var
+  PropInfo: PPropInfo;
+begin
+  PropInfo := FProps[TComponent(Sender).Tag];
+  with PropInfo^ do
+  begin
+    case PropType^.Kind of
+      tkEnumeration:
+        SetOrdProp(FSelectedObject, PropInfo, TComboBox(Sender).ItemIndex);
+    end;
+  end;
 end;
 
 constructor TSimplePropertyEditor.Create(AOwner: TComponent);
@@ -215,6 +230,8 @@ var
   Map: TIntegerMap;
   LoValue, HiValue: Single;
   P: PPropInfo;
+  TD: PTypeData;
+  S, SelName: string;
 const
   ROW_SPACE = 30;
   TOP_MARGIN = 20;
@@ -286,11 +303,16 @@ begin
       P := FProps[I];
       T1 := T;
       case P.PropType^.Kind of
+
         tkInteger, tkFloat:
           begin
             GetPropertyRange(AObject, P.Name, LoValue, HiValue);
 
             Control := TTrackBar.Create(nil);
+            Control.Parent := Self;
+            Control.Tag := I;
+            FPropertyControls.Add(Control);
+
             with TTrackBar(Control) do
             begin
               if P.PropType^.Kind = tkInteger then
@@ -317,9 +339,14 @@ begin
             Control.Left := 88;
             Control.Top := T + 4;
           end;
+
         tkClass:
           begin
             Control := TButton.Create(nil);
+            Control.Parent := Self;
+            Control.Tag := I;
+            FPropertyControls.Add(Control);
+
             TButton(Control).OnClick := ButtonHandler;
             TButton(Control).Caption := 'Edit';
             Control.Width := 60;
@@ -329,14 +356,39 @@ begin
             if GetObjectProp(FSelectedObject, P) = nil then
               Control.Enabled := False;
           end;
+
+        tkEnumeration:
+          begin
+            Control := TComboBox.Create(nil);
+            Control.Parent := Self;
+            Control.Tag := I;
+            FPropertyControls.Add(Control);
+
+            TComboBox(Control).OnChange := ComboBoxHandler;
+            TComboBox(Control).Style := csDropDownList;
+            Control.Width := 100;
+            Control.Height := 20;
+            Control.Left := 90;
+            Control.Top := T + 4;
+
+            SelName := GetEnumProp(FSelectedObject, P);
+            TD := GetTypeData(P.PropType^);
+
+            L := 0;
+            for K := TD.MinValue to TD.MaxValue do
+            begin
+              S := GetEnumName(P.PropType^, K);
+              if S = SelName then L := K;
+              TComboBox(Control).AddItem(S, nil);
+            end;
+
+            TComboBox(Control).ItemIndex := L;
+          end;
       else
         FPropertyControls.Add(nil);
         FLabels.Add(nil);
         Continue;
       end;
-      Control.Parent := Self;
-      Control.Tag := I;
-      FPropertyControls.Add(Control);
 
       ALabel := TLabel.Create(nil);
       ALabel.Caption := P.Name + ':';
