19cda50abeeeabc91654675a05186408c4595c1b
sekelsenmat <sekelsenmat@dadbf757-a53b-4b3f-9c79-7c22a95d7d7a>
2007-06-21 12:50:36 +0000

    misc lazarus fixes

--- trunk/GR32/Examples/Vcl/General/Mandelbrot_Ex/createbundle.sh
+++ trunk/GR32/Examples/Vcl/General/Mandelbrot_Ex/createbundle.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Force Bourne shell in case tcsh is default.
 #
-appname=MandelBrot_Ex
+appname=Mandelbrot_Ex
 appfolder=$appname.app
 macosfolder=$appfolder/Contents/MacOS
 plistfile=$appfolder/Contents/Info.plist
