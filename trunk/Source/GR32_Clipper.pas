373e7f40a1cd41ba6eec30a5193ed53e80c37ae6
angusj <angusj@dadbf757-a53b-4b3f-9c79-7c22a95d7d7a>
2012-06-04 22:24:09 +0000

    Updated license

--- trunk/Source/GR32_Clipper.pas
+++ trunk/Source/GR32_Clipper.pas
@@ -1,7 +1,7 @@
 unit GR32_Clipper;
 
 (* BEGIN LICENSE BLOCK *********************************************************
- * Version: MPL 1.1
+ * Version: MPL 1.1 or LGPL 2.1 with linking exception
  *
  * The contents of this file are subject to the Mozilla Public License Version
  * 1.1 (the "License"); you may not use this file except in compliance with
@@ -12,6 +12,13 @@ unit GR32_Clipper;
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
  * for the specific language governing rights and limitations under the
  * License.
+ *
+ * Alternatively, the contents of this file may be used under the terms of the
+ * Free Pascal modified version of the GNU Lesser General Public License
+ * Version 2.1 (the "FPC modified LGPL License"), in which case the provisions
+ * of this license are applicable instead of those above.
+ * Please see the file LICENSE.txt for additional information concerning this
+ * license.
  *
  * The Original Code is GR32_Clipper.
  * The Initial Developer of the Original Code is Angus Johnson and is
