999198a4220aa593a79ef7e37f6c08ea855b4b79
mandersson <meanderix@gmail.com>
2012-06-14 05:25:12 +0000

    Fixed license preambles and added new conditional symbols to GR32.INC (NOHINTING and NOHORIZONTALHINTING.)

--- Source/GR32_VPR2.pas
+++ Source/GR32_VPR2.pas
@@ -1,5 +1,37 @@
 unit GR32_VPR2;
 
+(* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1 or LGPL 2.1 with linking exception
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * Alternatively, the contents of this file may be used under the terms of the
+ * Free Pascal modified version of the GNU Lesser General Public License
+ * Version 2.1 (the "FPC modified LGPL License"), in which case the provisions
+ * of this license are applicable instead of those above.
+ * Please see the file LICENSE.txt for additional information concerning this
+ * license.
+ *
+ * The Original Code is Vectorial Polygon Rasterizer for Graphics32
+ *
+ * The Initial Developer of the Original Code is
+ * Mattias Andersson <mattias@centaurix.com>
+ *
+ * Portions created by the Initial Developer are Copyright (C) 2012
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * ***** END LICENSE BLOCK ***** *)
+
 interface
 
 {$I GR32.inc}
