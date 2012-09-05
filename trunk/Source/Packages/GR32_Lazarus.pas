8e980ad518b68c47b8725e3134b3310e774add3c
CWBudde <CWBudde@dadbf757-a53b-4b3f-9c79-7c22a95d7d7a>
2012-09-05 23:48:29 +0000

    * minor lazarus related changes

---
+++ trunk/Source/Packages/GR32_Lazarus.pas
@@ -0,0 +1,17 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit GR32_Lazarus;
+
+interface
+
+uses
+  GR32, GR32_Blend, GR32_Containers, GR32_ExtImage, GR32_Filters, GR32_Image, 
+  GR32_Layers, GR32_LowLevel, GR32_Math, GR32_MicroTiles, GR32_OrdinalMaps, 
+  GR32_Polygons, GR32_RangeBars, GR32_Rasterizers, GR32_RepaintOpt, 
+  GR32_Resamplers, GR32_System, GR32_Transforms, GR32_VectorMaps;
+
+implementation
+
+end.
