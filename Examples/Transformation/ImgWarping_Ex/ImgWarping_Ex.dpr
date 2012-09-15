d475aa54aee1e838af01bb1492b693bb0551926a
Christian Budde <Christian@savioursofsoul.de>
2012-09-15 19:47:33 +0000

    * further changes related to the new directory structure

--- Examples/Transformation/ImgWarping_Ex/ImgWarping_Ex.dpr
+++ Examples/Transformation/ImgWarping_Ex/ImgWarping_Ex.dpr
@@ -1,8 +1,9 @@
 program ImgWarping_Ex;
 
+{$R 'Media.res' 'Media.rc'}
+
 uses
   Forms,
-  GR32_MediaPathLocator in '..\..\GR32_MediaPathLocator.pas',
   MainUnit in 'MainUnit.pas' {MainForm},
   BrushAuxiliaries in 'BrushAuxiliaries.pas';
 
