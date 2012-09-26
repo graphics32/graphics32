dd28e58ac79628b2eb6ddd6f8bdfcb310f84036d
kusana <kusana@dadbf757-a53b-4b3f-9c79-7c22a95d7d7a>
2012-09-26 19:06:07 +0000

    Removed _Ex suffix from TextVPR example

---
+++ trunk/Examples/Drawing/TextVPR/TextVPR.dpr
@@ -0,0 +1,13 @@
+program TextVPR;
+
+{$R 'Media.res' 'Media.rc'}
+
+uses
+  Forms,
+  MainUnit in 'MainUnit.pas';
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TMainForm, MainForm);
+  Application.Run;
+end.
