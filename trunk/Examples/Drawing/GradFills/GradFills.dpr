0d2f932a45f10b0b60c386cd1729dfa5a230470d
kusana <kusana@dadbf757-a53b-4b3f-9c79-7c22a95d7d7a>
2012-09-26 18:59:09 +0000

    Removed _Ex suffix from GradFills example

---
+++ trunk/Examples/Drawing/GradFills/GradFills.dpr
@@ -0,0 +1,13 @@
+program GradFills;
+
+{$R 'Media.res' 'Media.rc'}
+
+uses
+  Forms,
+  MainUnit in 'MainUnit.pas';
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TMainForm, MainForm);
+  Application.Run;
+end.
