984bf5312cd0d514cbb3a1bf677fb403aa8a19b5
CWBudde <CWBudde@dadbf757-a53b-4b3f-9c79-7c22a95d7d7a>
2012-10-13 05:33:54 +0000

    * updated lion example (some minor changes)

--- trunk/Examples/Drawing/Lion/Lion.dpr
+++ trunk/Examples/Drawing/Lion/Lion.dpr
@@ -4,13 +4,13 @@ program Lion;
 
 uses
   Forms,
-  MainUnit in 'MainUnit.pas' {FrmTiger},
+  MainUnit in 'MainUnit.pas' {FrmLion},
   LionData in 'LionData.pas';
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TFrmTiger, FrmTiger);
+  Application.CreateForm(TFrmLion, FrmLion);
   Application.Run;
 end.
 
