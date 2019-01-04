4e59f708be0a44a626b7b284ccd24c9ad1bc643c
Michael Bunk <bunk@iat.uni-leipzig.de>
2019-01-04 08:01:32 +0100

    TWICImage is only available from Delphi 2010
    * see #44
    * http://dannywind.nl/tag/twicimage/
    * thanks for the notice, Bernd Karle

--- Source/GR32.pas
+++ Source/GR32.pas
@@ -2668,10 +2668,10 @@ procedure TCustomBitmap32.Assign(Source: TPersistent);
 {$IFNDEF PLATFORM_INDEPENDENT}
     else if SrcGraphic is TMetaFile then
       AssignFromGraphicMasked(TargetBitmap, SrcGraphic)
-{$IFDEF COMPILER2005_UP}
+    {$IFDEF COMPILER2010_UP}
     else if SrcGraphic is TWICImage then
       AssignFromGraphicPlain(TargetBitmap, SrcGraphic, 0, False)
-{$ENDIF}
+    {$ENDIF}
 {$ENDIF}
     else
       AssignFromGraphicPlain(TargetBitmap, SrcGraphic, clWhite32, True);
