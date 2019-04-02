68170b9bb8dd66318b3d5936ecd2e60f31027d28
Christian <Christian@savioursofsoul.de>
2019-04-03 00:51:22 +0200

    Added Mandelbrot CBuilder stub (uses the MandelUnit Delphi form)

---
+++ Examples/General/Mandelbrot/Mandelbrot.cpp
@@ -0,0 +1,34 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+#include <tchar.h>
+#include "MandelUnit.hpp"
+//---------------------------------------------------------------------------
+int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
+{
+	try
+	{
+		Application->Initialize();
+		Application->MainFormOnTaskBar = true;
+		Application->CreateForm(__classid(TMainForm), &MainForm);
+		Application->Run();
+	}
+	catch (Exception &exception)
+	{
+		Application->ShowException(&exception);
+	}
+	catch (...)
+	{
+		try
+		{
+			throw Exception("");
+		}
+		catch (Exception &exception)
+		{
+			Application->ShowException(&exception);
+		}
+	}
+	return 0;
+}
+//---------------------------------------------------------------------------
