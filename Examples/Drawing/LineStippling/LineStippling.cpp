61082e119dfb52f991e5bd3f7e1603fd923a6853
Christian <Christian@savioursofsoul.de>
2019-04-03 14:02:13 +0200

    Improved CBuilder Line Stippling example (might not work with older CBuilder versions, please complain if not!)

--- Examples/Drawing/LineStippling/LineStippling.cpp
+++ Examples/Drawing/LineStippling/LineStippling.cpp
@@ -3,20 +3,32 @@
 #include <vcl.h>
 #pragma hdrstop
 USERES("LineStippling.res");
-USEFORMNS("MainUnit.pas", Mainunit, Form1);
+USEFORMNS("MainUnit.pas", Mainunit, FormLineStippling);
 //---------------------------------------------------------------------------
-WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 {
-        try
-        {
-                 Application->Initialize();
-                 Application->CreateForm(__classid(TForm1), &Form1);
-                 Application->Run();
-        }
-        catch (Exception &exception)
-        {
-                 Application->ShowException(&exception);
-        }
-        return 0;
+	try
+	{
+		Application->Initialize();
+		Application->MainFormOnTaskBar = true;
+		Application->CreateForm(__classid(TFormLineStippling), &FormLineStippling);
+		Application->Run();
+	}
+	catch (Exception &exception)
+	{
+		Application->ShowException(&exception);
+	}
+	catch (...)
+	{
+		try
+		{
+			throw Exception("");
+		}
+		catch (Exception &exception)
+		{
+			Application->ShowException(&exception);
+		}
+	}
+	return 0;
 }
 //---------------------------------------------------------------------------
