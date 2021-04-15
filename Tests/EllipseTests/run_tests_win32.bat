26c36c3873b9bb29a2e96d9108a7ba7acba6ae02
lars <lars.scheme@gmail.com>
2021-04-15 22:09:57 +0200

    Rename ellipse tests run script

--- Tests/EllipseTests/run_tests_win32.bat
+++ Tests/EllipseTests/run_tests_win32.bat
@@ -1,3 +1,5 @@
+@REM This script expects Delphi to be on the PATH and uses the Win32 libraries.
+
 @call rsvars.bat
 @dcc32.exe --no-config ^
 -AForms=Vcl.Forms;Graphics=Vcl.Graphics;Controls=Vcl.Controls;Dialogs=Vcl.Dialogs;ExtCtrls=Vcl.ExtCtrls;Clipbrd=Vcl.Clipbrd ^
