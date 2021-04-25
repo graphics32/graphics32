feaf173df8c70357abf766c32566212df8675955
lars <lars.scheme@gmail.com>
2021-04-25 18:57:58 +0200

    Add batch script to run ellipse tests on Win 64

--- Tests/EllipseTests/run_tests_win64.bat
+++ Tests/EllipseTests/run_tests_win64.bat
@@ -1,11 +1,11 @@
-@REM This script expects Delphi to be on the PATH and uses the Win32 libraries.
+@REM This script expects Delphi to be on the PATH and uses the Win64 libraries.
 
 @call rsvars.bat
-@dcc32.exe --no-config ^
+@dcc64.exe --no-config ^
 -B ^
 -AForms=Vcl.Forms;Graphics=Vcl.Graphics;Controls=Vcl.Controls;Dialogs=Vcl.Dialogs;ExtCtrls=Vcl.ExtCtrls;Clipbrd=Vcl.Clipbrd ^
 -NSSystem;Winapi ^
--U"%BDS%\lib\Win32\release" ^
+-U"%BDS%\lib\Win64\release" ^
 EllipseTests.dpr
 @if %errorlevel% neq 0 (pause & exit)
 
