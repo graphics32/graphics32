c04bc2c2a7401102e9c3d3da2606f4a8f1a88d1c
Christian-W. Budde <Christian@pcjv.de>
2021-06-29 19:44:03 +0200

    Removed unnecessary dependency

--- Examples/Drawing/Lion/LionData.pas
+++ Examples/Drawing/Lion/LionData.pas
@@ -3,7 +3,7 @@ unit LionData;
 interface
 
 uses
-  {$IFNDEF FPC} Windows, {$ENDIF} SysUtils, GR32, GR32_VectorUtils, GR32_Paths;
+  SysUtils, GR32, GR32_VectorUtils, GR32_Paths;
 
 type
   TColoredPolygon = record
