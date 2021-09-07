d946bce195f7a60d5071d9e41788e282dc09eb78
Anders Melander <anders@melander.dk>
2021-09-07 23:23:26 +0200

    Added bitmap load/save unit test.

---
+++ Tests/Bitmap Test/BitmapTest.dpr
@@ -0,0 +1,40 @@
+program BitmapTest;
+{
+
+  Delphi DUnit Test Project
+  -------------------------
+  This project contains the DUnit test framework and the GUI/Console test runners.
+  Add "CONSOLE_TESTRUNNER" to the conditional defines entry in the project options
+  to use the console test runner.  Otherwise the GUI test runner will be used by
+  default.
+
+}
+
+{$IFDEF CONSOLE_TESTRUNNER}
+{$APPTYPE CONSOLE}
+{$ENDIF}
+
+{$R 'bitmaps.res' 'bitmaps.rc'}
+
+uses
+  TestFramework,
+  GUITestRunner,
+  TextTestRunner,
+  DUnitTestRunner,
+  Forms,
+  TestBitmapLoadSave in 'TestBitmapLoadSave.pas',
+  GR32 in '..\..\Source\GR32.pas',
+  FileTestFramework in 'FileTestFramework.pas';
+
+{$R *.RES}
+
+begin
+  Application.Initialize;
+  if IsConsole then
+    with TextTestRunner.RunRegisteredTests do
+      Free
+  else
+    GUITestRunner.RunRegisteredTests;
+end.
+
+
---
+++ Tests/Bitmap Test/TestBitmapLoadSave.pas
@@ -0,0 +1,203 @@
+unit TestBitmapLoadSave;
+
+interface
+
+uses
+  Classes, Types,
+  FileTestFramework,
+  GR32;
+
+type
+  TTestTCustomBitmap32 = class(TFileTestCase)
+  strict private
+    FBitmap32: TCustomBitmap32;
+    FExpectedCrc: Cardinal;
+    FIgnoreRes: boolean;
+  private
+    procedure TestSaveToStream(TopDown: boolean);
+    procedure ValidateCRC(Bitmap: TCustomBitmap32);
+  public
+    procedure SetUp; override;
+    procedure TearDown; override;
+  published
+    procedure TestLoadFromFile;
+    procedure TestLoadFromStream;
+    procedure TestLoadFromResourceName;
+    procedure TestSaveToStreamTopDown;
+    procedure TestSaveToStreamBottomUp;
+    procedure TestSaveToFile;
+  end;
+
+implementation
+
+uses
+  SysUtils,
+  IOUtils,
+  TestFramework,
+  ZLib; // CRC32
+
+const
+  // CRC32 checksum of TBitmap32 pixel data after load.
+  // If the checksum match then we assume that the size of the bitmap and the pixel colors match.
+  Checksums: array[0..27] of record
+    Name: string;
+    Checksum: Cardinal;
+    IgnoreRes: boolean;
+  end = (
+    (Name: 'pal1';      Checksum: $EB74525F;    IgnoreRes: False),
+    (Name: 'pal1bg';    Checksum: $461E3E42;    IgnoreRes: False),
+    (Name: 'pal1wb';    Checksum: $EB74525F;    IgnoreRes: False),
+    (Name: 'pal4';      Checksum: $963878B9;    IgnoreRes: False),
+    (Name: 'pal4gs';    Checksum: $38C07071;    IgnoreRes: False),
+    (Name: 'pal4rle';   Checksum: $963878B9;    IgnoreRes: False),
+    (Name: 'pal8-0';    Checksum: $A66B800E;    IgnoreRes: False),
+    (Name: 'pal8';      Checksum: $A66B800E;    IgnoreRes: False),
+    (Name: 'pal8gs';    Checksum: $FCFD7A5A;    IgnoreRes: False),
+    (Name: 'pal8nonsquare'; Checksum: $9C51A082;IgnoreRes: False),
+    (Name: 'pal8os2';   Checksum: $216B88F1;    IgnoreRes: False),
+    (Name: 'pal8rle';   Checksum: $A66B800E;    IgnoreRes: False),
+    (Name: 'pal8topdown'; Checksum: $980784BA;  IgnoreRes: False),
+    (Name: 'pal8v4';    Checksum: $A66B800E;    IgnoreRes: False),
+    (Name: 'pal8v5';    Checksum: $A66B800E;    IgnoreRes: False),
+    (Name: 'pal8w124';  Checksum: $C587558C;    IgnoreRes: False),
+    (Name: 'pal8w125';  Checksum: $28FB03E9;    IgnoreRes: False),
+    (Name: 'pal8w126';  Checksum: $7E8FACEE;    IgnoreRes: False),
+    (Name: 'rgb16-565'; Checksum: $2B0C3870;    IgnoreRes: False),
+    (Name: 'rgb16-565pal'; Checksum: $2B0C3870; IgnoreRes: True), // MS Resource Compiler alters bitmap
+    (Name: 'rgb16';     Checksum: $C2A5F1C7;    IgnoreRes: False),
+    (Name: 'rgb16bfdef';Checksum: $C2A5F1C7;    IgnoreRes: False),
+    (Name: 'rgb24';     Checksum: $0BDF42DF;    IgnoreRes: False),
+    (Name: 'rgb24pal';  Checksum: $0BDF42DF;    IgnoreRes: False),
+    (Name: 'rgb32';     Checksum: $0BDF42DF;    IgnoreRes: False),
+    (Name: 'rgb32bf';   Checksum: $0BDF42DF;    IgnoreRes: False),
+    (Name: 'rgb32bfdef';Checksum: $0BDF42DF;    IgnoreRes: False),
+    // Additional
+    (Name: 'rgb32fakealpha';Checksum: $23821D77;IgnoreRes: False));
+
+procedure TTestTCustomBitmap32.SetUp;
+begin
+  FBitmap32 := TBitmap32.Create;
+
+  var Name := TPath.GetFileNameWithoutExtension(TestFileName);
+  var Found := False;
+
+  for var Checksum in Checksums do
+    if (SameText(Checksum.Name, Name)) then
+    begin
+      FExpectedCrc := Checksum.Checksum;
+      FIgnoreRes := Checksum.IgnoreRes;
+      Found := True;
+      break;
+    end;
+
+  Check(Found, Format('%s not found in CRC list', [Name]));
+end;
+
+procedure TTestTCustomBitmap32.TearDown;
+begin
+  FBitmap32.Free;
+  FBitmap32 := nil;
+end;
+
+procedure TTestTCustomBitmap32.ValidateCRC(Bitmap: TCustomBitmap32);
+begin
+  var Crc: Cardinal := crc32(0, nil, 0);
+  Crc := crc32(Crc, PByte(Bitmap.Bits), Bitmap.Width*Bitmap.Height*SizeOf(DWORD));
+
+  CheckEquals(Crc, FExpectedCrc, 'Bitmap checksum validation failed');
+end;
+
+procedure TTestTCustomBitmap32.TestLoadFromStream;
+begin
+  var Stream := TFileStream.Create(TestFileName, fmOpenRead or fmShareDenyWrite);
+  try
+
+    FBitmap32.Clear;
+    FBitmap32.LoadFromStream(Stream);
+
+  finally
+    Stream.Free;
+  end;
+
+  ValidateCRC(FBitmap32);
+  Check(not FBitmap32.Empty);
+end;
+
+procedure TTestTCustomBitmap32.TestLoadFromFile;
+begin
+  FBitmap32.Clear;
+  FBitmap32.LoadFromFile(TestFileName);
+
+  ValidateCRC(FBitmap32);
+  Check(not FBitmap32.Empty);
+end;
+
+procedure TTestTCustomBitmap32.TestLoadFromResourceName;
+begin
+  var ResName := TPath.GetFileNameWithoutExtension(TestFileName);
+  ResName := ResName.Replace('-', '', [rfReplaceAll]);
+
+  FBitmap32.Clear;
+  FBitmap32.LoadFromResourceName(HInstance, ResName);
+
+  if (not FIgnoreRes) then
+    ValidateCRC(FBitmap32);
+  Check(not FBitmap32.Empty);
+end;
+
+procedure TTestTCustomBitmap32.TestSaveToStream(TopDown: boolean);
+begin
+  FBitmap32.LoadFromFile(TestFileName);
+
+  var Stream := TMemoryStream.Create;
+  try
+
+    FBitmap32.SaveToStream(Stream, TopDown);
+    Stream.Position := 0;
+
+    FBitmap32.Clear;
+    FBitmap32.LoadFromStream(Stream);
+
+  finally
+    Stream.Free;
+  end;
+
+  ValidateCRC(FBitmap32);
+  Check(not FBitmap32.Empty);
+end;
+
+procedure TTestTCustomBitmap32.TestSaveToStreamTopDown;
+begin
+  TestSaveToStream(True);
+end;
+
+procedure TTestTCustomBitmap32.TestSaveToStreamBottomUp;
+begin
+  TestSaveToStream(False);
+end;
+
+procedure TTestTCustomBitmap32.TestSaveToFile;
+begin
+  FBitmap32.LoadFromFile(TestFileName);
+
+  var NewFilename := TGUID.NewGuid.ToString + '.bmp';
+  FBitmap32.SaveToFile(NewFilename, True);
+  try
+
+    FBitmap32.Clear;
+    FBitmap32.LoadFromFile(NewFilename);
+
+  finally
+    TFile.Delete(NewFilename);
+  end;
+
+  ValidateCRC(FBitmap32);
+  Check(not FBitmap32.Empty);
+end;
+
+initialization
+  var TestSuite := TFolderTestSuite.Create('Load and save bitmap', TTestTCustomBitmap32, '.\Data', '*.bmp', True);
+  RegisterTest(TestSuite);
+end.
+
+
