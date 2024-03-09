c07b8387f2958e2855b2ceab0a73cf5927c13575
Anders Melander <anders@melander.dk>
2024-03-10 00:02:54 +0100

    Updated packages with new unit names.

--- Source/GR32_Dsgn_Misc.pas
+++ Source/GR32_Dsgn_Misc.pas
@@ -73,12 +73,9 @@ type
 
 implementation
 
-uses GR32, GR32_Resamplers;
-                        
-{$IFDEF COMPILER2005_UP}
-var
-  GSplashScreen     : HBITMAP;
-{$ENDIF}
+uses
+  GR32,
+  GR32_Resamplers;
 
 { TCustomClassProperty }
 
@@ -183,12 +180,12 @@ begin
   TBitmap32(GetComponent(0)).ResamplerClassName := CustomClass;
 end;
 
+var
+  GSplashScreen     : HBITMAP;
+
 initialization
-  {$IFDEF COMPILER2005_UP}
   // Add Splash Screen
   GSplashScreen := LoadBitmap(hInstance, 'GR32');
-  (SplashScreenServices as IOTasplashScreenServices).AddPluginBitmap(
-      'GR32' + ' ' + Graphics32Version, GSplashScreen);
-  {$ENDIF}
+  (SplashScreenServices as IOTasplashScreenServices).AddPluginBitmap('Graphics32', GSplashScreen, False, 'Open Source', Graphics32Version);
 
 end.
