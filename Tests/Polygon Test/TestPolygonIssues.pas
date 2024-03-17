07da6a68948d1e19ceb82286924efa7f878f4a9c
Anders Melander <anders@melander.dk>
2024-03-17 02:51:42 +0100

    Added unit regression test for RoundRect missing point

--- Tests/Polygon Test/TestPolygonIssues.pas
+++ Tests/Polygon Test/TestPolygonIssues.pas
@@ -12,13 +12,16 @@ type
   public
   published
     procedure Issue272_PolygonFS;
+    procedure RoundRect_MissingPoint;
   end;
 
 implementation
 
 uses
   SysUtils,
+  Math,
   GR32,
+  GR32_VectorUtils,
   GR32_Polygons;
 
 {$RANGECHECKS OFF}
@@ -77,6 +80,73 @@ begin
   DoTest(p, 0.1);
 end;
 
+
+procedure TTestPolygonIssues.RoundRect_MissingPoint;
+var
+  i: integer;
+  Found: boolean;
+begin
+  // Verify that RoundRect produces a polygon aligned with the X and Y axis
+  var Rect := FloatRect(10, 10, 30, 20);
+  var RoundedRectangle := RoundRect(Rect, 2.0);
+
+  // Look for horizontal segment going from (x1,10) to (x2,10)
+  Found := False;
+  for i := 0 to High(RoundedRectangle) do
+    if (RoundedRectangle[i].Y = Rect.Top) then
+    begin
+      Found := SameValue(Rect.Top, RoundedRectangle[(i+1) mod Length(RoundedRectangle)].Y);
+      if (Found) then
+      begin
+        CheckNotEquals(RoundedRectangle[i].X, RoundedRectangle[(i+1) mod Length(RoundedRectangle)].X);
+        break;
+      end;
+    end;
+  Check(Found);
+
+  // Look for horizontal segment going from (x1,20) to (x2,20)
+  Found := False;
+  for i := 0 to High(RoundedRectangle) do
+    if (RoundedRectangle[i].Y = Rect.Bottom) then
+    begin
+      Found := SameValue(Rect.Bottom, RoundedRectangle[(i+1) mod Length(RoundedRectangle)].Y);
+      if (Found) then
+      begin
+        CheckNotEquals(RoundedRectangle[i].X, RoundedRectangle[(i+1) mod Length(RoundedRectangle)].X);
+        break;
+      end;
+    end;
+  Check(Found);
+
+  // Look for vertical segment going from (10,y1) to (10,y2)
+  Found := False;
+  for i := 0 to High(RoundedRectangle) do
+    if (RoundedRectangle[i].X = Rect.Left) then
+    begin
+      Found := SameValue(Rect.Left, RoundedRectangle[(i+1) mod Length(RoundedRectangle)].X);
+      if (Found) then
+      begin
+        CheckNotEquals(RoundedRectangle[i].Y, RoundedRectangle[(i+1) mod Length(RoundedRectangle)].Y);
+        break;
+      end;
+    end;
+  Check(Found);
+
+  // Look for vertical segment going from (30,y1) to (30,y2)
+  Found := False;
+  for i := 0 to High(RoundedRectangle) do
+    if (RoundedRectangle[i].X = Rect.Right) then
+    begin
+      Found := SameValue(Rect.Right, RoundedRectangle[(i+1) mod Length(RoundedRectangle)].X);
+      if (Found) then
+      begin
+        CheckNotEquals(RoundedRectangle[i].Y, RoundedRectangle[(i+1) mod Length(RoundedRectangle)].Y);
+        break;
+      end;
+    end;
+  Check(Found);
+
+end;
 
 initialization
   RegisterTest(TTestPolygonIssues.Suite);
