d5c70f9d9d0105a430764d6b037a41eaa7ba219e
Anders Melander <anders@melander.dk>
2024-09-26 22:55:27 +0200

    Updated PixelCombine example to demonstrate the new composition system

--- Examples/Blending/PixelCombine/MainUnit.dfm
+++ Examples/Blending/PixelCombine/MainUnit.dfm
@@ -2,64 +2,128 @@ object FormPixelCombine: TFormPixelCombine
   Left = 295
   Top = 110
   Caption = 'PixelCombine Example'
-  ClientHeight = 346
-  ClientWidth = 564
+  ClientHeight = 361
+  ClientWidth = 763
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
   Font.Height = -11
   Font.Name = 'MS Sans Serif'
   Font.Style = []
-  OldCreateOrder = False
   OnCreate = FormCreate
-  DesignSize = (
-    564
-    346)
-  PixelsPerInch = 96
   TextHeight = 13
+  object Splitter1: TSplitter
+    Left = 575
+    Top = 0
+    Height = 361
+    Align = alRight
+    ResizeStyle = rsUpdate
+    ExplicitLeft = 636
+    ExplicitTop = 176
+    ExplicitHeight = 100
+  end
   object ImgView: TImgView32
-    Left = 16
-    Top = 20
-    Width = 393
-    Height = 309
-    Anchors = [akLeft, akTop, akRight, akBottom]
+    Left = 0
+    Top = 0
+    Width = 575
+    Height = 361
+    Align = alClient
+    Bitmap.DrawMode = dmBlend
+    Bitmap.CombineMode = cmMerge
     Bitmap.ResamplerClassName = 'TNearestResampler'
     BitmapAlign = baCustom
     Color = clBtnShadow
     ParentColor = False
     Scale = 1.000000000000000000
     ScaleMode = smScale
-    ScrollBars.ShowHandleGrip = True
-    ScrollBars.Style = rbsDefault
+    Background.FillStyle = bfsCheckers
+    Background.CheckersStyle = bcsMedium
+    ScrollBars.Increment = 0
     ScrollBars.Size = 16
     OverSize = 0
     TabOrder = 0
+    OnMouseDown = ImgViewMouseDown
   end
-  object RadioGroup: TRadioGroup
-    Left = 415
-    Top = 20
-    Width = 141
-    Height = 285
-    Anchors = [akTop, akRight]
-    Caption = 'Operation'
-    ItemIndex = 0
-    Items.Strings = (
-      'Opaque (none)'
-      'Add / Plus / Lighter'
-      'Sub'
-      'Modulate / Multiply'
-      'Min / Darken'
-      'Max / Lighten'
-      'Screen'
-      'Color-Dodge'
-      'Color-Burn'
-      'Difference'
-      'Exclusion'
-      'Pattern'
-      'Blend'
-      'Blend Add'
-      'Blend Modulate')
+  object PanelOptions: TPanel
+    Left = 578
+    Top = 0
+    Width = 185
+    Height = 361
+    Align = alRight
+    BevelOuter = bvNone
+    ShowCaption = False
     TabOrder = 1
-    OnClick = RadioGroupClick
+    object ListViewOperation: TListView
+      Left = 0
+      Top = 49
+      Width = 185
+      Height = 312
+      Align = alClient
+      Checkboxes = True
+      Columns = <
+        item
+          AutoSize = True
+        end>
+      GroupView = True
+      ReadOnly = True
+      RowSelect = True
+      TabOrder = 0
+      ViewStyle = vsReport
+      OnSelectItem = ListViewOperationSelectItem
+      OnItemChecked = ListViewOperationItemChecked
+    end
+    object PanelLayer: TPanel
+      Left = 0
+      Top = 0
+      Width = 185
+      Height = 29
+      Align = alTop
+      BevelOuter = bvNone
+      TabOrder = 1
+      DesignSize = (
+        185
+        29)
+      object Label1: TLabel
+        Left = 6
+        Top = 7
+        Width = 29
+        Height = 13
+        Caption = 'Layer:'
+        FocusControl = ComboBoxLayer
+      end
+      object ComboBoxLayer: TComboBox
+        Left = 48
+        Top = 4
+        Width = 109
+        Height = 21
+        Style = csDropDownList
+        Anchors = [akLeft, akTop, akRight]
+        TabOrder = 0
+        OnSelect = ComboBoxLayerSelect
+      end
+      object ButtonLayerAdd: TButton
+        Left = 160
+        Top = 4
+        Width = 21
+        Height = 21
+        Anchors = [akTop, akRight]
+        Caption = '+'
+        TabOrder = 1
+        OnClick = ButtonLayerAddClick
+      end
+    end
+    object TrackBarAlpha: TTrackBar
+      Left = 0
+      Top = 29
+      Width = 185
+      Height = 20
+      Align = alTop
+      Max = 255
+      TabOrder = 2
+      ThumbLength = 15
+      TickMarks = tmBoth
+      TickStyle = tsNone
+      OnChange = TrackBarAlphaChange
+    end
   end
 end
--- Examples/Blending/PixelCombine/MainUnit.pas
+++ Examples/Blending/PixelCombine/MainUnit.pas
@@ -38,36 +38,49 @@ interface
 
 uses
   {$IFNDEF FPC} Windows, {$ELSE} LCLIntf, LCLType, LResources, {$ENDIF}
-  SysUtils, Classes, Graphics, Controls, Forms, Dialogs, StdCtrls, ExtCtrls,
+  SysUtils, Classes, Graphics, Controls, Forms, Dialogs, StdCtrls, ExtCtrls, ComCtrls,
+  Generics.Collections,
   GR32,
+  GR32_Blend,
+  GR32.Blend.Modes,
   GR32_Image,
   GR32_Layers;
 
 type
   TFormPixelCombine = class(TForm)
     ImgView: TImgView32;
-    RadioGroup: TRadioGroup;
+    Splitter1: TSplitter;
+    PanelOptions: TPanel;
+    ListViewOperation: TListView;
+    PanelLayer: TPanel;
+    ComboBoxLayer: TComboBox;
+    Label1: TLabel;
+    TrackBarAlpha: TTrackBar;
+    ButtonLayerAdd: TButton;
     procedure FormCreate(Sender: TObject);
-    procedure RadioGroupClick(Sender: TObject);
+    procedure ListViewOperationItemChecked(Sender: TObject; Item: TListItem);
+    procedure ListViewOperationSelectItem(Sender: TObject; Item: TListItem; Selected: Boolean);
+    procedure ComboBoxLayerSelect(Sender: TObject);
+    procedure ImgViewMouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer; Layer: TCustomLayer);
+    procedure TrackBarAlphaChange(Sender: TObject);
+    procedure ButtonLayerAddClick(Sender: TObject);
   protected
-    procedure PC_Add(F: TColor32; var B: TColor32; M: Cardinal);
-    procedure PC_Sub(F: TColor32; var B: TColor32; M: Cardinal);
-    procedure PC_Modulate(F: TColor32; var B: TColor32; M: Cardinal);
-    procedure PC_Min(F: TColor32; var B: TColor32; M: Cardinal);
-    procedure PC_Max(F: TColor32; var B: TColor32; M: Cardinal);
-    procedure PC_Screen(F: TColor32; var B: TColor32; M: Cardinal);
-    procedure PC_ColorBurn(F: TColor32; var B: TColor32; M: Cardinal);
-    procedure PC_ColorDodge(F: TColor32; var B: TColor32; M: Cardinal);
-    procedure PC_Difference(F: TColor32; var B: TColor32; M: Cardinal);
-    procedure PC_Exclusion(F: TColor32; var B: TColor32; M: Cardinal);
-    procedure PC_Pattern(F: TColor32; var B: TColor32; M: Cardinal);
-    procedure PC_Blend(F: TColor32; var B: TColor32; M: Cardinal);
-    procedure PC_BlendAdd(F: TColor32; var B: TColor32; M: Cardinal);
-    procedure PC_BlendModulate(F: TColor32; var B: TColor32; M: Cardinal);
   private
-    FPatCount: Integer;
-    FLayer1: TBitmapLayer;
-    FLayer2: TBitmapLayer;
+    FRubberbandLayer: TRubberbandLayer;
+    FBlenders: TObjectDictionary<TBitmapLayer, TCustomGraphics32Blender>;
+    FCurrentItem: TListItem;
+    FPatternCount: integer;
+
+    function AddLayer(const Name: string; DrawMode: TDrawMode): TBitmapLayer;
+
+    function GetCurrentLayer: TBitmapLayer;
+    procedure SetCurrentLayer(const Value: TBitmapLayer);
+
+    property CurrentLayer: TBitmapLayer read GetCurrentLayer write SetCurrentLayer;
+
+  public
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
   end;
 
 var
@@ -79,13 +92,52 @@ implementation
 
 uses
   Types,
-  GR32_Blend,
+  Math,
   GR32_RangeBars,
-  GR32.ImageFormats.JPG;
+  GR32.ImageFormats.JPG,
+  GR32_VectorUtils,
+  GR32_Polygons,
+  GR32.Blend.Modes.PhotoShop,
+  GR32.Blend.Modes.PorterDuff;
+
+const
+  BitmapSize = 200;
+  BitmapOffset = 20;
 
 { TFormPixelCombine }
 
-procedure TFormPixelCombine.FormCreate(Sender: TObject);
+constructor TFormPixelCombine.Create(AOwner: TComponent);
+begin
+  inherited;
+  FBlenders := TObjectDictionary<TBitmapLayer, TCustomGraphics32Blender>.Create([doOwnsValues]);
+end;
+
+destructor TFormPixelCombine.Destroy;
+begin
+  FBlenders.Free;
+  inherited;
+end;
+
+function TFormPixelCombine.AddLayer(const Name: string; DrawMode: TDrawMode): TBitmapLayer;
+begin
+  // Add new layer but ensure rubberband remains the last layer
+  if (FRubberbandLayer <> nil) then
+    Result := TBitmapLayer(ImgView.Layers.Insert(FRubberbandLayer.Index, TBitmapLayer))
+  else
+    Result := TBitmapLayer.Create(ImgView.Layers);
+
+  Result.Visible := False;
+  Result.Bitmap.DrawMode := DrawMode;
+  Result.Bitmap.CombineMode := cmMerge;
+
+  ComboBoxLayer.Items.AddObject(Name, Result);
+
+  FBlenders.Add(Result, nil);
+
+  Result.Visible := True;
+end;
+
+procedure TFormPixelCombine.ButtonLayerAddClick(Sender: TObject);
 
   procedure GenerateBitmap(Bitmap: TBitmap32);
   var
@@ -93,6 +145,9 @@ procedure TFormPixelCombine.FormCreate(Sender: TObject);
     SinY, SinX: Double;
     Color: TColor32;
   begin
+    Assert(Bitmap.Height <= 255); // Prevent DivMul255Table overflow
+    Assert(Bitmap.Width <= 255);
+
     // Just a pattern with some variation
     for Y := 0 to Bitmap.Height-1 do
     begin
@@ -102,191 +157,301 @@ procedure TFormPixelCombine.FormCreate(Sender: TObject);
       begin
         SinX := Sin(X * 0.1);
 
+        // 2D sine wave pattern
         Color :=  Gray32(Round(((SinX + SinY) * 0.25 + 0.5) * 255));
         // Alpha gradient
-        Color := SetAlpha(Color, MulDiv(255, Y, Bitmap.Height-1));
-        
-        Bitmap[X, Y] := Color;    
+        Color := SetAlpha(Color, DivMul255Table[Bitmap.Height-1, Y]); // MulDiv(255, Y, Bitmap.Height-1)
+
+        Bitmap[X, Y] := Color;
       end;
     end;
   end;
 
 var
-  RubberbandLayer: TRubberbandLayer;
+  Layer: TBitmapLayer;
   r: TRect;
   Viewport: TRect;
-  Location: TFloatRect;
-const
-  BitmapSize = 200;
-  BitmapOffset = 20;
 begin
-  // Load background picture 'Runner'
-  ImgView.Bitmap.LoadFromResourceName(HInstance, 'Runner', RT_RCDATA);
+  Inc(FPatternCount);
+  Layer := AddLayer(Format('Pattern %d', [FPatternCount]), dmCustom);
+  Layer.Scaled := True;
+  Layer.Bitmap.SetSize(BitmapSize, BitmapSize);
 
-  // Create foreground bitmap layers
-  
-  // First layer is unscaled
-  FLayer1 := TBitmapLayer.Create(ImgView.Layers);
-  FLayer1.Visible := False;
-  FLayer1.Bitmap.SetSize(BitmapSize, BitmapSize);
-  FLayer1.Bitmap.DrawMode := dmCustom;
-  GenerateBitmap(FLayer1.Bitmap);
-  FLayer1.Scaled := False;
-  // Position top-left
-  r := FLayer1.Bitmap.BoundsRect;
-  r.Offset(BitmapOffset, BitmapOffset);
-  if (FLayer1.Scaled) then
-    // Location is relative to bitmap
-    Location := ImgView.ControlToBitmap(r)
-  else
-    // Location is relative to viewport
-    Location := FloatRect(r);
-  FLayer1.Location := Location;
+  GenerateBitmap(Layer.Bitmap);
 
-  // Second layer is scaled
-  FLayer2 := TBitmapLayer.Create(ImgView.Layers);
-  FLayer2.Visible := False;
-  FLayer2.Bitmap.Assign(FLayer1.Bitmap);
-  FLayer2.Scaled := True;
-  // Position bottom-right
-  r := FLayer1.Bitmap.BoundsRect;
+  // Random position within viewport
+  r := Layer.Bitmap.BoundsRect;
   Viewport := ImgView.GetViewportRect;
-  r.Offset(Viewport.Width-r.Width-BitmapOffset, Viewport.Height-r.Height-BitmapOffset);
-  if (FLayer2.Scaled) then
-    // Location is relative to bitmap
-    Location := ImgView.ControlToBitmap(r)
+  r.Offset(Random(Viewport.Width - Layer.Bitmap.Width), Random(Viewport.Height - Layer.Bitmap.Height));
+  Layer.Location := ImgView.ControlToBitmap(r);
+
+  CurrentLayer := Layer;
+end;
+
+procedure TFormPixelCombine.ComboBoxLayerSelect(Sender: TObject);
+begin
+  if (ComboBoxLayer.ItemIndex <> -1) then
+    CurrentLayer := TBitmapLayer(ComboBoxLayer.Items.Objects[ComboBoxLayer.ItemIndex])
   else
-    // Location is relative to viewport
-    Location := FloatRect(r);
-  FLayer2.Location := Location;
-
-
-  // Create rubberband layers so we can move the foreground layers around
-  RubberbandLayer := TRubberbandLayer.Create(ImgView.Layers);
-  RubberbandLayer.Visible := False;
-  RubberbandLayer.ChildLayer := FLayer1;
-  RubberbandLayer.Handles := [rhCenter, rhFrame, rhCorners];
-  RubberbandLayer.ChildLayer.Visible := True;
-  RubberbandLayer.Visible := True;
-
-  RubberbandLayer := TRubberbandLayer.Create(ImgView.Layers);
-  RubberbandLayer.Visible := False;
-  RubberbandLayer.ChildLayer := FLayer2;
-  RubberbandLayer.Handles := [rhCenter, rhFrame, rhCorners];
-  RubberbandLayer.ChildLayer.Visible := True;
-  RubberbandLayer.Visible := True;
+    CurrentLayer := nil;
 end;
 
-procedure TFormPixelCombine.PC_Add(F: TColor32; var B: TColor32; M: Cardinal);
-begin
-  B := ColorAdd(F, B);
-end;
+procedure TFormPixelCombine.FormCreate(Sender: TObject);
 
-procedure TFormPixelCombine.PC_Max(F: TColor32; var B: TColor32; M: Cardinal);
-begin
-  B := ColorMax(F, B);
-end;
-
-procedure TFormPixelCombine.PC_Min(F: TColor32; var B: TColor32; M: Cardinal);
-begin
-  B := ColorMin(F, B);
-end;
-
-procedure TFormPixelCombine.PC_Modulate(F: TColor32; var B: TColor32; M: Cardinal);
-begin
-  B := ColorModulate(F, B);
-end;
-
-procedure TFormPixelCombine.PC_Pattern(F: TColor32; var B: TColor32; M: Cardinal);
-begin
-  FPatCount := 1 - FPatCount;
-  if FPatCount = 0 then
-    B := F;
-end;
-
-procedure TFormPixelCombine.PC_Sub(F: TColor32; var B: TColor32; M: Cardinal);
-begin
-  B := ColorSub(F, B);
-end;
-
-procedure TFormPixelCombine.PC_Screen(F: TColor32; var B: TColor32; M: Cardinal);
-begin
-  B := ColorScreen(F, B);
-end;
-
-procedure TFormPixelCombine.PC_ColorDodge(F: TColor32; var B: TColor32; M: Cardinal);
-begin
-  B := ColorDodge(F, B);
-end;
-
-procedure TFormPixelCombine.PC_ColorBurn(F: TColor32; var B: TColor32; M: Cardinal);
-begin
-  B := ColorBurn(F, B);
-end;
-
-procedure TFormPixelCombine.PC_Difference(F: TColor32; var B: TColor32; M: Cardinal);
-begin
-  B := ColorDifference(F, B);
-end;
-
-procedure TFormPixelCombine.PC_Exclusion(F: TColor32; var B: TColor32; M: Cardinal);
-begin
-  B := ColorExclusion(F, B);
-end;
-
-procedure TFormPixelCombine.PC_Blend(F: TColor32; var B: TColor32; M: Cardinal);
-begin
-  B := BlendReg(F, B);
-end;
-
-procedure TFormPixelCombine.PC_BlendAdd(F: TColor32; var B: TColor32; M: Cardinal);
-begin
-  B := BlendColorAdd(F, B);
-end;
-
-procedure TFormPixelCombine.PC_BlendModulate(F: TColor32; var B: TColor32; M: Cardinal);
-begin
-  B := BlendColorModulate(F, B);
-end;
-
-procedure TFormPixelCombine.RadioGroupClick(Sender: TObject);
-begin
-  case RadioGroup.ItemIndex of
-    0:
-      FLayer1.Bitmap.OnPixelCombine := nil;
-    1:
-      FLayer1.Bitmap.OnPixelCombine := PC_Add;
-    2:
-      FLayer1.Bitmap.OnPixelCombine := PC_Sub;
-    3:
-      FLayer1.Bitmap.OnPixelCombine := PC_Modulate;
-    4:
-      FLayer1.Bitmap.OnPixelCombine := PC_Min;
-    5:
-      FLayer1.Bitmap.OnPixelCombine := PC_Max;
-    6:
-      FLayer1.Bitmap.OnPixelCombine := PC_Screen;
-    7:
-      FLayer1.Bitmap.OnPixelCombine := PC_ColorDodge;
-    8:
-      FLayer1.Bitmap.OnPixelCombine := PC_ColorBurn;
-    9:
-      FLayer1.Bitmap.OnPixelCombine := PC_Difference;
-    10:
-      FLayer1.Bitmap.OnPixelCombine := PC_Exclusion;
-    11:
-      FLayer1.Bitmap.OnPixelCombine := PC_Pattern;
-    12:
-      FLayer1.Bitmap.OnPixelCombine := PC_Blend;
-    13:
-      FLayer1.Bitmap.OnPixelCombine := PC_BlendAdd;
-    14:
-      FLayer1.Bitmap.OnPixelCombine := PC_BlendModulate;
+  procedure DrawCircle(ABitmap: TBitmap32; Alpha: integer = 255);
+  var
+    Shape: TArrayOfFloatPoint;
+  begin
+    ABitmap.SetSize(BitmapSize, BitmapSize, True);
+    Shape := Circle(ABitmap.Width / 2, ABitmap.Height / 2, Min(ABitmap.Width, ABitmap.Height) / 2 - 4);
+    PolygonFS(ABitmap, Shape, SetAlpha(clOrange32, Alpha), pfWinding);
+    PolylineFS(ABitmap, Shape, clYellow32, True, 4);
   end;
-  FLayer2.Bitmap.OnPixelCombine := FLayer1.Bitmap.OnPixelCombine;
-  
-  FLayer1.Changed;
-  FLayer2.Changed;
+
+  procedure DrawBox(ABitmap: TBitmap32; Alpha: integer = 255);
+  var
+    r: TRect;
+    Shape: TArrayOfFloatPoint;
+  begin
+    ABitmap.SetSize(BitmapSize, BitmapSize, True);
+    r := ABitmap.BoundsRect;
+    GR32.InflateRect(r, -10, -10);
+    Shape := GR32_VectorUtils.Rectangle(r);
+    PolygonFS(ABitmap, Shape, SetAlpha(clLightSkyblue32, Alpha), pfWinding);
+    PolylineFS(ABitmap, Shape, clBlue32, True, 4);
+  end;
+
+var
+  Layer: TBitmapLayer;
+  r: TRect;
+  Location: TFloatRect;
+  Group: IGraphics32BlendGroup;
+  ListGroup: TListGroup;
+  Item: TListItem;
+  BlenderClass: TGraphics32BlenderClass;
+begin
+  // Note: Layer.Scaled makes no difference in this concrete example since
+  // we have no bitmap (in ImgView.Bitmap) to scale/position relative to.
+
+  (*
+  ** Create unscaled background bitmap layer
+  *)
+  Layer := AddLayer('(background)', dmOpaque);
+  Layer.Scaled := False;
+  Layer.Bitmap.LoadFromResourceName(HInstance, 'Runner', RT_RCDATA);
+  Layer.Bitmap.CombineMode := cmBlend;
+  // Center, relative to viewport
+  Location := FloatRect(ImgView.GetViewportRect);
+  Location.Inflate(-Location.Width / 4, -Location.Height / 4);
+  Layer.Location := Location;
+
+
+  (*
+  ** Create scaled foreground bitmap layers
+  *)
+
+  // First layer: Circle
+  Layer := AddLayer('Layer 1', dmCustom);
+  Layer.Scaled := True;
+  DrawCircle(Layer.Bitmap, 255);
+  // Position top-left, relative to bitmap
+  r := Layer.Bitmap.BoundsRect;
+  r.Offset(r.Width div 4, r.Height div 4);
+  Layer.Location := ImgView.ControlToBitmap(r);
+
+  // Second layer: Box
+  Layer := AddLayer('Layer 2', dmCustom);
+  Layer.Scaled := True;
+  DrawBox(Layer.Bitmap, 255);
+  // Position bottom-right, relative to bitmap
+  r := Layer.Bitmap.BoundsRect;
+  r.Offset(r.Width - r.Width div 4, r.Height - r.Height div 4);
+  Layer.Location := ImgView.ControlToBitmap(r);
+
+
+  (*
+  ** Create rubberband layer so we can move the foreground layers around
+  *)
+  FRubberbandLayer := TRubberbandLayer.Create(ImgView.Layers);
+  FRubberbandLayer.Visible := False;
+  FRubberbandLayer.Handles := [rhCenter, rhFrame, rhCorners];
+
+
+  (*
+  ** Populate blend mode list
+  *)
+  FCurrentItem := pointer(-1);
+  ListViewOperation.Items.BeginUpdate;
+  try
+
+    for Group in Graphics32BlendService.Groups do
+    begin
+      ListGroup := ListViewOperation.Groups.Add;
+      ListGroup.Header := Group.Name;
+
+      for BlenderClass in Group do
+      begin
+        Item := ListViewOperation.Items.Add;
+        Item.Caption := BlenderClass.Name;
+        Item.SubItems.Add(BlenderClass.ID);
+        Item.Data := pointer(BlenderClass);
+        Item.GroupID := ListGroup.GroupID;
+      end;
+    end;
+
+  finally
+    ListViewOperation.Items.EndUpdate;
+  end;
+  FCurrentItem := nil;
+end;
+
+function TFormPixelCombine.GetCurrentLayer: TBitmapLayer;
+begin
+  if (FRubberbandLayer.ChildLayer <> nil) then
+    Result := TBitmapLayer(FRubberbandLayer.ChildLayer)
+  else
+    Result := nil;
+end;
+
+procedure TFormPixelCombine.ImgViewMouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer;
+  Layer: TCustomLayer);
+begin
+  if (Layer is TBitmapLayer) then
+    CurrentLayer := TBitmapLayer(Layer);
+end;
+
+procedure TFormPixelCombine.SetCurrentLayer(const Value: TBitmapLayer);
+var
+  i: integer;
+  Blender: TCustomGraphics32Blender;
+  Found: boolean;
+begin
+  if (FRubberbandLayer.ChildLayer = Value) then
+    exit; // Guards against endless recursion via ComboBoxLayer
+
+  FRubberbandLayer.ChildLayer := Value;
+  FRubberbandLayer.Visible := (Value <> nil);
+
+  Found := False;
+
+  if (Value <> nil) and (Value.Bitmap.DrawMode <> dmOpaque) then
+  begin
+    ListViewOperation.Enabled := (Value.Bitmap.DrawMode <> dmOpaque);
+    TrackBarAlpha.Position := Value.Bitmap.MasterAlpha;
+
+    Blender := FBlenders[Value];
+
+    if (Blender <> nil) then
+    begin
+      for i := 0 to ListViewOperation.Items.Count-1 do
+        if (TGraphics32BlenderClass(ListViewOperation.Items[i].Data) = Blender.ClassType) then
+        begin
+          ListViewOperation.Items[i].Checked := True;
+          ListViewOperation.Items[i].MakeVisible(False);
+          Found := True;
+          break;
+        end;
+
+    end;
+
+    TrackBarAlpha.Position := Value.Bitmap.MasterAlpha;
+    TrackBarAlpha.Enabled := True;
+  end else
+  begin
+    ListViewOperation.Enabled := False;
+    TrackBarAlpha.Position := 0;
+    TrackBarAlpha.Enabled := False;
+  end;
+
+  if (not Found) then
+  begin
+    FCurrentItem := pointer(-1);
+
+    for i := 0 to ListViewOperation.Items.Count-1 do
+      ListViewOperation.Items[i].Checked := False;
+
+    FCurrentItem := nil;
+  end;
+
+  if (Value <> nil) then
+    ComboBoxLayer.ItemIndex := ComboBoxLayer.Items.IndexOfObject(Value)
+  else
+    ComboBoxLayer.ItemIndex := -1;
+end;
+
+procedure TFormPixelCombine.TrackBarAlphaChange(Sender: TObject);
+begin
+  if (CurrentLayer <> nil) then
+    CurrentLayer.Bitmap.MasterAlpha := TrackBarAlpha.Position;
+end;
+
+procedure TFormPixelCombine.ListViewOperationItemChecked(Sender: TObject; Item: TListItem);
+var
+  i: integer;
+  Layer: TBitmapLayer;
+  BlenderClass: TGraphics32BlenderClass;
+  CurrentBlender: TCustomGraphics32Blender;
+  NewBlender: TCustomGraphics32Blender;
+  BlendFunc: TPixelCombineEvent;
+begin
+  if (FCurrentItem <> nil) then
+    exit; // Avoid recursion
+
+  FCurrentItem := Item;
+  try
+    // Can only uncheck by checking another item
+    if (not FCurrentItem.Checked) then
+    begin
+      FCurrentItem.Checked := True;
+      exit;
+    end;
+
+    for i := 0 to ListViewOperation.Items.Count-1 do
+      if (Item <> ListViewOperation.Items[i]) then
+        ListViewOperation.Items[i].Checked := False;
+
+  finally
+    FCurrentItem := nil;
+  end;
+
+  Layer := CurrentLayer;
+  if (Layer = nil) then
+    exit;
+
+  BlenderClass := TGraphics32BlenderClass(Item.Data);
+  CurrentBlender := FBlenders[Layer];
+
+  if (CurrentBlender <> nil) and (CurrentBlender.ClassType = BlenderClass) then
+    exit; // Same blender; Do nothing
+
+  NewBlender := TGraphics32BlenderClass(Item.Data).Create;
+  try
+
+    // Replace existing blender with new one
+    FBlenders.Items[Layer] := NewBlender;
+
+  except
+    NewBlender.Free;
+    raise;
+  end;
+
+  NewBlender.GetPixelCombiner(BlendFunc);
+
+  Layer.Bitmap.OnPixelCombine := BlendFunc;
+
+  if (Assigned(BlendFunc)) then
+    Layer.Bitmap.DrawMode := dmCustom
+  else
+    // No blend function implies that we should use the standard blend op
+    Layer.Bitmap.DrawMode := dmBlend;
+
+  Layer.Changed;
+end;
+
+procedure TFormPixelCombine.ListViewOperationSelectItem(Sender: TObject; Item: TListItem; Selected: Boolean);
+begin
+  if (Selected) then
+    Item.Checked := True;
 end;
 
 end.
