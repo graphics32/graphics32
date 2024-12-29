c80b0a5e30eda19ff6d896423a15e8829d0bc8b5
Anders Melander <anders@melander.dk>
2024-12-29 18:21:07 +0100

    Added Resampler unit test

---
+++ Tests/Resampler test/ResamplerTest.dpr
@@ -0,0 +1,36 @@
+program ResamplerTest;
+{
+
+  Delphi DUnit Test Project
+  -------------------------
+  This project contains the DUnit test framework and the GUI/Console test runners.
+  Add "CONSOLE_TESTRUNNER" to the conditional defines entry in the project options
+  to use the console test runner.  Otherwise the GUI test runner will be used by
+  default.
+
+}
+
+{$IFDEF CONSOLE_TESTRUNNER}
+{$APPTYPE CONSOLE}
+{$ENDIF}
+
+uses
+  TestFramework,
+  GUITestRunner,
+  TextTestRunner,
+  DUnitTestRunner,
+  Forms,
+  TestResampler in 'TestResampler.pas';
+
+{$R *.RES}
+
+begin
+  Application.Initialize;
+  if IsConsole then
+    with TextTestRunner.RunRegisteredTests do
+      Free
+  else
+    GUITestRunner.RunRegisteredTests;
+end.
+
+
