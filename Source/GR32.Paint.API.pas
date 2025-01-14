03403a0174d90453aa984cfcdbb5980eb867de2d
Anders Melander <anders@melander.dk>
2025-01-14 04:54:20 +0100

    Moved all paint logic out of form and into controller classes.
    Still a bit to do before it's done...

--- Source/GR32.Paint.API.pas
+++ Source/GR32.Paint.API.pas
@@ -37,7 +37,9 @@ interface
 uses
   Classes,
   Controls,
-  GR32;
+  GR32,
+  GR32_Layers,
+  GR32.Paint.Tool.API;
 
 
 //------------------------------------------------------------------------------
@@ -45,7 +47,8 @@ uses
 //      Paint tool settings
 //
 //------------------------------------------------------------------------------
-// Not presently implemented
+// Not presently implemented.
+// TODO : Move to GR32.Paint.Settings.API
 //------------------------------------------------------------------------------
 type
   ISettingValues = interface
@@ -59,43 +62,21 @@ type
 
 //------------------------------------------------------------------------------
 //
-//      Paint tool mouse event params
-//
-//------------------------------------------------------------------------------
-type
-  TBitmap32PaintToolMouseParams = record
-    MouseMessageTime: Cardinal;         // Timestamp
-    ScreenPos: TPoint;                  // Screen coordinates
-    ViewPortPos: TPoint;                // Viewport coordinates
-    BitmapPos: TPoint;                  // Bitmap coordinates, rounded down
-    BitmapPosSnap: TPoint;              // Bitmap coordinates, snapped to nearest.
-    BitmapPosFloat: TFloatPoint;        // Fractional bitmap coordinates
-    ShiftState: TShiftState;            // Mouse/keyboard shift state
-  end;
-
-  PBitmap32PaintToolMouseParams = ^TBitmap32PaintToolMouseParams;
-
-type
-  IBitmap32PaintToolContext = interface
-    ['{BE0ECC3B-F27B-4689-A7A8-55958EA4047C}']
-    function GetMouseParams: PBitmap32PaintToolMouseParams;
-    property MouseParams: PBitmap32PaintToolMouseParams read GetMouseParams;
-
-    function GetBuffer: TBitmap32;
-    property Buffer: TBitmap32 read GetBuffer;
-  end;
-
-
-//------------------------------------------------------------------------------
-//
 //      IBitmap32PaintHost
 //
 //------------------------------------------------------------------------------
-// Provides an interface between the host application and the paint tools.
+// Handles coordinate space conversion, colors, layers, cursor, etc.
+//------------------------------------------------------------------------------
+// Provides the paint tools with an interface to the host application.
 //------------------------------------------------------------------------------
 type
   IBitmap32PaintHost = interface
     ['{D51C1D86-A06D-4E3C-95B4-9F70ADD7A9DF}']
+    function GetPaintLayer: TBitmapLayer;
+    procedure SetPaintLayer(const Value: TBitmapLayer);
+    property PaintLayer: TBitmapLayer read GetPaintLayer write SetPaintLayer;
+
+    // TODO : Move to PaintController
     function GetColorPrimary: TColor32;
     procedure SetColorPrimary(const Value: TColor32);
     function GetColorSecondary: TColor32;
@@ -109,48 +90,20 @@ type
 
     function ViewPortToScreen(const APoint: TPoint): TPoint;
     function ScreenToViewPort(const APoint: TPoint): TPoint;
-    function ViewPortToBitmap(const APoint: TPoint; SnapToNearest: boolean = True): TPoint;
+    function ViewPortToBitmap(const APoint: TPoint; SnapToNearest: boolean = True): TPoint; overload;
+    function ViewPortToBitmap(const APoint: TFloatPoint): TFloatPoint; overload;
     function BitmapToViewPort(const APoint: TPoint): TPoint;
 
     function GetToolSettings(const AToolKey: string): ISettingValues;
 
-    function CreateToolContext(const ViewPortPos: TPoint; SnapMouse: boolean): IBitmap32PaintToolContext;
+    // CreateToolContext creates a new tool context object.
+    // It is the responsibility of the caller to initialize and maintain the returned object with context values.
+    function CreateToolContext(const APaintTool: IBitmap32PaintTool): IBitmap32PaintToolContext;
 
     // SetToolVectorCursor: Called from tools to set complex cursors.
     function SetToolVectorCursor(const Polygon: TArrayOfFixedPoint; HotspotX, HotspotY: integer; Color: TColor32; const OutlinePattern: TArrayOfColor32): boolean;
     // Changed: Called from tools. Used for undo management.
     procedure Changed(const Action: string);
-  end;
-
-
-//------------------------------------------------------------------------------
-//
-//      IBitmap32PaintExtension
-//
-//------------------------------------------------------------------------------
-// Base interface for bitmap tools (tools, actions, filters, color pickers, etc)
-//------------------------------------------------------------------------------
-type
-  IBitmap32PaintExtension = interface
-    ['{17AF7811-406F-4C58-906C-C3E6E3FC67D2}']
-    function GetIsVisible: boolean;
-    function GetIsEnabled: boolean;
-    function GetCaption: string;
-    function GetHint: string;
-    function GetDescription: string;
-    function GetAttribution: string;
-
-    // Clear instructs the extension to delete all allocated resources.
-    procedure Clear;
-    // Reset instructs the extension to reset its setting to their default values.
-    procedure Reset;
-
-    property IsVisible: boolean read GetIsVisible;
-    property IsEnabled: boolean read GetIsEnabled;
-    property Caption: string read GetCaption;
-    property Hint: string read GetHint;
-    property Description: string read GetDescription;
-    property Attribution: string read GetAttribution;
   end;
 
 
@@ -164,14 +117,16 @@ type
 type
   TBitmap32PaintToolContext = class(TInterfacedObject, IBitmap32PaintToolContext)
   private
+    FPaintHost: IBitmap32PaintHost;
     FMouseParams: TBitmap32PaintToolMouseParams;
     FBuffer: TBitmap32;
   private
     // IBitmap32PaintToolContext
     function GetMouseParams: PBitmap32PaintToolMouseParams;
     function GetBuffer: TBitmap32;
+    procedure Update(const ViewPortPos: TPoint; SnapMouse: boolean);
   public
-    constructor Create(ABuffer: TBitmap32);
+    constructor Create(const APaintHost: IBitmap32PaintHost; ABuffer: TBitmap32);
   end;
 
 //------------------------------------------------------------------------------
@@ -185,9 +140,10 @@ implementation
 //      TBitmap32PaintToolContext
 //
 //------------------------------------------------------------------------------
-constructor TBitmap32PaintToolContext.Create(ABuffer: TBitmap32);
+constructor TBitmap32PaintToolContext.Create(const APaintHost: IBitmap32PaintHost; ABuffer: TBitmap32);
 begin
   inherited Create;
+  FPaintHost := APaintHost;
   FBuffer := ABuffer;
   FMouseParams := Default(TBitmap32PaintToolMouseParams);
 end;
@@ -195,6 +151,18 @@ end;
 function TBitmap32PaintToolContext.GetMouseParams: PBitmap32PaintToolMouseParams;
 begin
   Result := @FMouseParams;
+end;
+
+procedure TBitmap32PaintToolContext.Update(const ViewPortPos: TPoint; SnapMouse: boolean);
+begin
+  FMouseParams.ViewPortPos := ViewPortPos;
+  FMouseParams.ScreenPos := FPaintHost.ViewPortToScreen(ViewPortPos);
+
+  FMouseParams.BitmapPosFloat := FPaintHost.ViewPortToBitmap(FloatPoint(ViewPortPos));
+
+  // If SnapMouse=True then we only return the snapped coordinates
+  FMouseParams.BitmapPos := FPaintHost.ViewPortToBitmap(ViewPortPos, SnapMouse);
+  FMouseParams.BitmapPosSnap := FPaintHost.ViewPortToBitmap(ViewPortPos, True);
 end;
 
 function TBitmap32PaintToolContext.GetBuffer: TBitmap32;
