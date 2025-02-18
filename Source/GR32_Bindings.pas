5766450f3927bf67e587cd5d4786396b14ce41da
Anders Melander <anders@melander.dk>
2025-02-18 01:25:04 +0100

    Bindings changes for FPC

--- Source/GR32_Bindings.pas
+++ Source/GR32_Bindings.pas
@@ -258,9 +258,15 @@ type
     function FindBinding(BindVariable: PPointer): IBindingInfo; overload;
     function FindBinding(FunctionID: NativeInt): IBindingInfo; overload;
 
-    property Bindings[const Name: string]: IBindingInfo read GetBinding; default;
     property Bindings[BindVariable: PPointer]: IBindingInfo read GetBinding; default;
+{$ifndef FPC}
     property Bindings[FunctionID: NativeInt]: IBindingInfo read GetBinding; default;
+    property Bindings[const Name: string]: IBindingInfo read GetBinding; default;
+{$else} // Lazarus 2.6/FPC 3.0 broke support for overloaded properties. See FPC #15384
+    property BindingsByName[const Name: string]: IBindingInfo read GetBinding;
+    property BindingsByID[FunctionID: NativeInt]: IBindingInfo read GetBinding;
+{$endif}
+
 
     // List of bindings in this registry.
     function GetEnumerator: TEnumerator<IBindingInfo>;
