b4a94f49f8870f2e9a96fccd37835257e8388236
Anders Melander <anders@melander.dk>
2025-02-24 11:30:53 +0100

    Fixed leak in AntiAliasing example

--- Examples/Drawing/AntiAliasing/MainUnit.pas
+++ Examples/Drawing/AntiAliasing/MainUnit.pas
@@ -138,90 +138,97 @@ begin
 
     // Top patterns
     LinGrad := TLinearGradientPolygonFiller.Create;
-    LinGrad.Gradient.StartColor := clWhite32;
-    for Index := 1 to 20 do
-    begin
-      Renderer.Color := clWhite32;
-      Renderer.Filler := nil;
+    try
 
-      // integral point sizes 1..20
-      Outline := Circle(20.5 + Index * (Index + 1), 20.5, 0.5 * Index, 8 + Index);
-      Renderer.PolygonFS(Outline);
+      LinGrad.Gradient.StartColor := clWhite32;
 
-      // fractional point sizes 0..2
-      Outline := Circle(18.5 + 4 * Index, 33.5, 0.05 * Index, 8);
-      Renderer.PolygonFS(Outline);
-
-      // fractional point positioning
-      Outline := Circle(18.4 + 4.1 * Index, 27.4 + 0.1 * Index, 0.5, 8);
-      Renderer.PolygonFS(Outline);
-
-      Renderer.Filler := LinGrad;
-      Points[0] := FloatPoint(20 + Index * (Index + 1), 40.5);
-      Points[1] := FloatPoint(20 + Index * (Index + 1) + (Index - 1) * 4, 100.5);
-      LinGrad.SimpleGradient(Points[0], clWhite32, Points[1], IndexToColor32(Index));
-      Outline := BuildPolyline(Points, Index, jsRound, esRound);
-      Renderer.PolygonFS(Outline);
-
-      // fractional line lengths H (red/blue)
-      LinGrad.Gradient.StartColor := $FF0000FF;
-      LinGrad.Gradient.EndColor := $FFFF0000;
-
-      Points[0] := FloatPoint(17.5 + 4 * Index, 107);
-      Points[1] := FloatPoint(17.5 + 4.15 * Index, 107);
-      LinGrad.SetPoints(Points[0], Points[1]);
-      Renderer.PolygonFS(BuildPolyline(Points, 1));
-
-      // fractional line lengths V (red/blue)
-      Points[0] := FloatPoint(18 + 4 * Index, 112.5);
-      Points[1] := FloatPoint(18 + 4.15 * Index, 112.5 + Index * 0.15);
-      LinGrad.SetPoints(Points[0], Points[1]);
-      Renderer.PolygonFS(BuildPolyline(Points, 1));
-
-      // fractional line positioning (red)
-      Points[0] := FloatPoint(21.5, 120 + (Index - 1) * 3.1);
-      Points[1] := FloatPoint(52.5, 120 + (Index - 1) * 3.1);
-      LinGrad.SimpleGradient(Points[0], $FFFF0000, Points[1], clWhite32);
-      Renderer.PolygonFS(BuildPolyline(Points, 1));
-
-      // fractional line width 2..0 (green)
-      Points[0] := FloatPoint(52.5, 118 + Index * 3);
-      Points[1] := FloatPoint(83.5, 118 + Index * 3);
-      LinGrad.SimpleGradient(Points[0], $FF00FF00, Points[1], clWhite32);
-      Outline := BuildPolyline(Points, 2 - (Index - 1) * 0.1, jsRound, esRound);
-      Renderer.PolygonFS(Outline);
-
-      // stippled fractional width 2..0 (blue)
-      Points[0] := FloatPoint(83.5, 119 + Index * 3);
-      Points[1] := FloatPoint(114.5, 119 + Index * 3);
-      LinGrad.SimpleGradient(Points[0], $FF0000FF, Points[1], clWhite32);
-      Renderer.PolyPolygonFS(BuildPolyPolyline(BuildDashedLine(Points,
-        ArrayOfFloat([3, 3])), False, 2 - (Index - 1) * 0.1));
-
-      Renderer.Color := clWhite32;
-      Renderer.Filler := nil;
-
-      // integral line width, horz aligned (mipmap test)
-      if Index < 10 then
+      for Index := 1 to 20 do
       begin
-        Points[0] := FloatPoint(125.5, 119.5 + (Index + 2) * (Index * 0.5));
-        Points[1] := FloatPoint(135.5, 119.5 + (Index + 2) * (Index * 0.5));
+        Renderer.Color := clWhite32;
+        Renderer.Filler := nil;
+
+        // integral point sizes 1..20
+        Outline := Circle(20.5 + Index * (Index + 1), 20.5, 0.5 * Index, 8 + Index);
+        Renderer.PolygonFS(Outline);
+
+        // fractional point sizes 0..2
+        Outline := Circle(18.5 + 4 * Index, 33.5, 0.05 * Index, 8);
+        Renderer.PolygonFS(Outline);
+
+        // fractional point positioning
+        Outline := Circle(18.4 + 4.1 * Index, 27.4 + 0.1 * Index, 0.5, 8);
+        Renderer.PolygonFS(Outline);
+
+        Renderer.Filler := LinGrad;
+        Points[0] := FloatPoint(20 + Index * (Index + 1), 40.5);
+        Points[1] := FloatPoint(20 + Index * (Index + 1) + (Index - 1) * 4, 100.5);
+        LinGrad.SimpleGradient(Points[0], clWhite32, Points[1], IndexToColor32(Index));
         Outline := BuildPolyline(Points, Index, jsRound, esRound);
         Renderer.PolygonFS(Outline);
+
+        // fractional line lengths H (red/blue)
+        LinGrad.Gradient.StartColor := $FF0000FF;
+        LinGrad.Gradient.EndColor := $FFFF0000;
+
+        Points[0] := FloatPoint(17.5 + 4 * Index, 107);
+        Points[1] := FloatPoint(17.5 + 4.15 * Index, 107);
+        LinGrad.SetPoints(Points[0], Points[1]);
+        Renderer.PolygonFS(BuildPolyline(Points, 1));
+
+        // fractional line lengths V (red/blue)
+        Points[0] := FloatPoint(18 + 4 * Index, 112.5);
+        Points[1] := FloatPoint(18 + 4.15 * Index, 112.5 + Index * 0.15);
+        LinGrad.SetPoints(Points[0], Points[1]);
+        Renderer.PolygonFS(BuildPolyline(Points, 1));
+
+        // fractional line positioning (red)
+        Points[0] := FloatPoint(21.5, 120 + (Index - 1) * 3.1);
+        Points[1] := FloatPoint(52.5, 120 + (Index - 1) * 3.1);
+        LinGrad.SimpleGradient(Points[0], $FFFF0000, Points[1], clWhite32);
+        Renderer.PolygonFS(BuildPolyline(Points, 1));
+
+        // fractional line width 2..0 (green)
+        Points[0] := FloatPoint(52.5, 118 + Index * 3);
+        Points[1] := FloatPoint(83.5, 118 + Index * 3);
+        LinGrad.SimpleGradient(Points[0], $FF00FF00, Points[1], clWhite32);
+        Outline := BuildPolyline(Points, 2 - (Index - 1) * 0.1, jsRound, esRound);
+        Renderer.PolygonFS(Outline);
+
+        // stippled fractional width 2..0 (blue)
+        Points[0] := FloatPoint(83.5, 119 + Index * 3);
+        Points[1] := FloatPoint(114.5, 119 + Index * 3);
+        LinGrad.SimpleGradient(Points[0], $FF0000FF, Points[1], clWhite32);
+        Renderer.PolyPolygonFS(BuildPolyPolyline(BuildDashedLine(Points,
+          ArrayOfFloat([3, 3])), False, 2 - (Index - 1) * 0.1));
+
+        Renderer.Color := clWhite32;
+        Renderer.Filler := nil;
+
+        // integral line width, horz aligned (mipmap test)
+        if Index < 10 then
+        begin
+          Points[0] := FloatPoint(125.5, 119.5 + (Index + 2) * (Index * 0.5));
+          Points[1] := FloatPoint(135.5, 119.5 + (Index + 2) * (Index * 0.5));
+          Outline := BuildPolyline(Points, Index, jsRound, esRound);
+          Renderer.PolygonFS(Outline);
+        end;
+
+        // fractional line positioning, 1 px H
+        Points[0] := FloatPoint(17.5 + 4.1 * Index - 0.1, 186);
+        Points[1] := FloatPoint(18.5 + 4.1 * Index - 0.1, 186);
+        Renderer.PolygonFS(BuildPolyline(Points, 1));
+
+        // fractional line width 0..2, 1 px H
+        Points[0] := FloatPoint(17.5 + 4 * Index, 192);
+        Points[1] := FloatPoint(18.5 + 4 * Index, 192);
+        Renderer.PolygonFS(BuildPolyline(Points, 0.1 * Index));
       end;
 
-      // fractional line positioning, 1 px H
-      Points[0] := FloatPoint(17.5 + 4.1 * Index - 0.1, 186);
-      Points[1] := FloatPoint(18.5 + 4.1 * Index - 0.1, 186);
-      Renderer.PolygonFS(BuildPolyline(Points, 1));
+      DrawTriangles;
 
-      // fractional line width 0..2, 1 px H
-      Points[0] := FloatPoint(17.5 + 4 * Index, 192);
-      Points[1] := FloatPoint(18.5 + 4 * Index, 192);
-      Renderer.PolygonFS(BuildPolyline(Points, 0.1 * Index));
+    finally
+      LinGrad.Free;
     end;
-
-    DrawTriangles;
   finally
     Renderer.Free;
   end;
