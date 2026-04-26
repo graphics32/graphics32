bd386aec3c13818a059c9b5f51ba456805cd1845
Anders Melander <anders@melander.dk>
2026-04-26 17:31:33 +0200

    Fixed a few bugs in blend benchmark and added benchmark of early exit

--- Benchmarks/Blend.Merge/Benchmark.Blend.MergeMem.dpr
+++ Benchmarks/Blend.Merge/Benchmark.Blend.MergeMem.dpr
@@ -24,7 +24,27 @@ begin
       var ColorFG: TColor32 := F or (F shl 16);
       for var B := 0 to $FF do
       begin
-        var ColorBG: TColor32 := B or (B shl 8) or (B shl 16);
+        var ColorBG: TColor32 := B or (B shl 8) or (B shl 16) or (B shl 24);
+        BlendMemProc(ColorFG, ColorBG);
+      end;
+    end;
+  end;
+end;
+
+procedure BM_BlendMemExit(const state: TState);
+begin
+  var BlendMemProc := TBlendMem(state[0]);
+
+  for var _ in state do
+  begin
+    for var F_A: byte in [0, $80, $FF] do
+    for var F := 0 to $2FF do
+    begin
+      var ColorFG: TColor32 := F or (F shl 16) or (F_A shl 24);
+      for var B_A: byte in [0, $80, $FF] do
+      for var B := 0 to $FF do
+      begin
+        var ColorBG: TColor32 := B or (B shl 8) or (B shl 16) or (B_A shl 24);
         BlendMemProc(ColorFG, ColorBG);
       end;
     end;
@@ -42,7 +62,27 @@ begin
       var ColorFG: TColor32 := F or (F shl 16);
       for var B := 0 to $FF do
       begin
-        var ColorBG: TColor32 := B or (B shl 8) or (B shl 16);
+        var ColorBG: TColor32 := B or (B shl 8) or (B shl 16) or (B shl 24);
+        BlendRegProc(ColorFG, ColorBG);
+      end;
+    end;
+  end;
+end;
+
+procedure BM_BlendRegExit(const state: TState);
+begin
+  var BlendRegProc: TBlendReg := TBlendReg(state[0]);
+
+  for var _ in state do
+  begin
+    for var F_A: byte in [0, $80, $FF] do
+    for var F := 0 to $2FF do
+    begin
+      var ColorFG: TColor32 := F or (F shl 16) or (F_A shl 24);
+      for var B_A: byte in [0, $80, $FF] do
+      for var B := 0 to $FF do
+      begin
+        var ColorBG: TColor32 := B or (B shl 8) or (B shl 16) or (B_A shl 24);
         BlendRegProc(ColorFG, ColorBG);
       end;
     end;
@@ -54,6 +94,9 @@ procedure Main;
 begin
   Spring.Benchmark.benchmark_format_args := False;
 
+  (*
+  ** MergeMem
+  *)
   var Binding := BlendRegistry.FindBinding('MergeMem');
   Assert(Binding <> nil);
 
@@ -64,12 +107,29 @@ begin
     bm.TimeUnit(kMillisecond);
   end;
 
+  for var Implement in Binding do
+  begin
+    var bm := Spring.Benchmark.Benchmark(BM_BlendMemExit, Implement.Name+'(early exit)').Args([Int64(Implement.Proc)]);
+    bm.Iterations(1000);
+    bm.TimeUnit(kMillisecond);
+  end;
+
+  (*
+  ** MergeReg
+  *)
   Binding := BlendRegistry.FindBinding('MergeReg');
   Assert(Binding <> nil);
 
   for var Implement in Binding do
   begin
     var bm := Spring.Benchmark.Benchmark(BM_BlendReg, Implement.Name).Args([Int64(Implement.Proc)]);
+    bm.Iterations(1000);
+    bm.TimeUnit(kMillisecond);
+  end;
+
+  for var Implement in Binding do
+  begin
+    var bm := Spring.Benchmark.Benchmark(BM_BlendRegExit, Implement.Name+'(early exit)').Args([Int64(Implement.Proc)]);
     bm.Iterations(1000);
     bm.TimeUnit(kMillisecond);
   end;
