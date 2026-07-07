e666c32f1c5d74676776377f0f4bbc1364c1d2f0
Anders Melander <anders@melander.dk>
2026-07-07 14:16:29 +0200

    CheckParams related optimizations
    - ColorToGrayscale(Dst, Src) now doesn't clear the destination bitmap if it is resized since the content is completely replaced by the filter function anyway.
    - ApplyLUT(Dst, Src) now doesn't clear the destination bitmap and ignores the PreserveAlpha parameter if the destination bitmap was resized, since there is no alpha to preserve if the bitmap is resized.
      This is a breaking change for code that relied on the function to return a bitmap with Alpha=0 if the bitmap was resized. Unlikely but possible.

--- Source/GR32_Filters.pas
+++ Source/GR32_Filters.pas
@@ -465,7 +465,7 @@ var
   I: Integer;
   D, S: PColor32;
 begin
-  CheckParams(Dst, Src);
+  CheckParams(Dst, Src, True, False);
   D := PColor32(Dst.Bits);
   S := PColor32(Src.Bits);
 
@@ -498,7 +498,12 @@ var
   I: Integer;
   D, S: PColor32Entry;
 begin
-  CheckParams(Dst, Src);
+  //
+  // If CheckParams has to resize the destination bitmap then the bitmap
+  // is will be cleared and there is no alpha to preserve. Therefore we
+  // only preserve alpha if the bitmap is not resized.
+  //
+  PreserveAlpha := not CheckParams(Dst, Src, True, False) and PreserveAlpha;
   D := PColor32Entry(Dst.Bits);
   S := PColor32Entry(Src.Bits);
 
